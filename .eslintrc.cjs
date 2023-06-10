c69a442e9cc56a8e1a042c2f848992efd87c49bd
Nikita Tebloev <nikita.tebloev@polyus-nt.ru>
2023-06-10 22:34:51 +0800

    change configs, add monaco

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,43 +1,58 @@
+// module.exports = {
+//   root: true,
+//   env: {
+//     browser: true,
+//     commonjs: true,
+//     es6: true,
+//     node: true
+//   },
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true
+//     },
+//     sourceType: 'module',
+//     ecmaVersion: 2021
+//   },
+//   plugins: ['@typescript-eslint'],
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:react/recommended',
+//     'plugin:react/jsx-runtime',
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:@typescript-eslint/eslint-recommended',
+//     'plugin:prettier/recommended'
+//   ],
+//   rules: {
+//     '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+//     '@typescript-eslint/explicit-function-return-type': 'error',
+//     '@typescript-eslint/explicit-module-boundary-types': 'off',
+//     '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
+//     '@typescript-eslint/no-explicit-any': 'error',
+//     '@typescript-eslint/no-non-null-assertion': 'off',
+//     '@typescript-eslint/no-var-requires': 'off'
+//   },
+//   overrides: [
+//     {
+//       files: ['*.js'],
+//       rules: {
+//         '@typescript-eslint/explicit-function-return-type': 'off'
+//       }
+//     }
+//   ]
+// }
+
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    sourceType: 'module',
-    ecmaVersion: 2021
-  },
-  plugins: ['@typescript-eslint'],
+  env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:prettier/recommended'
+    'plugin:react-hooks/recommended'
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
   rules: {
-    '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-var-requires': 'off'
-  },
-  overrides: [
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off'
-      }
-    }
-  ]
-}
+    'react-refresh/only-export-components': 'warn'
+  }
+};

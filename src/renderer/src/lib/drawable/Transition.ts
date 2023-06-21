3eeb28238ffa9e2e2b7d0cf7b83f237039887529
Nikita Tebloev <nikita.tebloev@polyus-nt.ru>
2023-06-21 21:50:03 +0800

    change arrow styles

--- src/renderer/src/lib/drawable/Transition.ts
+++ src/renderer/src/lib/drawable/Transition.ts
@@ -1,6 +1,8 @@
 import { getBoxToBoxArrow } from 'curved-arrows';
 
 import { State } from './State';
+import { Vector2D } from '../types';
+import { rotatePoint } from '../utils';
 
 interface TransitionArgs {
   source: State;
@@ -16,6 +18,28 @@ export class Transition {
     this.target = args.target;
   }
 
+  private drawTriangle(
+    ctx: CanvasRenderingContext2D,
+    origin: Vector2D,
+    width: number,
+    height: number,
+    angle: number
+  ) {
+    const p1 = rotatePoint({ x: origin.x - width, y: origin.y - height / 2 }, origin, angle);
+    const p2 = rotatePoint({ x: origin.x - width, y: origin.y + height / 2 }, origin, angle);
+
+    ctx.beginPath();
+
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(origin.x, origin.y);
+    ctx.lineTo(p2.x, p2.y);
+    ctx.lineTo(p1.x, p1.y);
+
+    ctx.fill();
+
+    ctx.closePath();
+  }
+
   draw(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
     if (!this.source || !this.target) return;
 
@@ -29,7 +53,7 @@ export class Transition {
       this.target.bounds.width,
       this.target.bounds.height,
       {
-        padEnd: 0,
+        padEnd: 5,
       }
     );
 
@@ -41,7 +65,24 @@ export class Transition {
     ctx.moveTo(sx, sy);
     ctx.bezierCurveTo(c1x, c1y, c2x, c2y, ex, ey);
     ctx.stroke();
-
     ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(sx, sy, 5, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
+
+    const origin = { x: ex, y: ey };
+    if (ae === 0) {
+      origin.x += 11;
+    } else if (ae === 90) {
+      origin.y += 11;
+    } else if (ae === 180) {
+      origin.x -= 11;
+    } else if (ae === 270) {
+      origin.y -= 11;
+    }
+
+    this.drawTriangle(ctx, origin, 11, 11, (ae * Math.PI) / 180);
   }
 }
--- src/renderer/src/lib/utils.ts
+++ src/renderer/src/lib/utils.ts
@@ -1,3 +1,5 @@
+import { Vector2D } from './types';
+
 export const isPointInRectangle = (
   rectangle: {
     x: number;
@@ -110,4 +112,23 @@ export const isRectanglesIntersection = (
   }
 
   return false;
+};
+
+export const rotatePoint = (point: Vector2D, origin: Vector2D, angle: number) => {
+  const s = Math.sin(angle);
+  const c = Math.cos(angle);
+
+  // translate point back to origin:
+  point.x -= origin.x;
+  point.y -= origin.y;
+
+  // rotate point
+  const xNew = point.x * c - point.y * s;
+  const yNew = point.x * s + point.y * c;
+
+  // translate point back:
+  point.x = xNew + origin.x;
+  point.y = yNew + origin.y;
+
+  return point;
 };

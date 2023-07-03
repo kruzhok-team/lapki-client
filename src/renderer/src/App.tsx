f73fbc18376d9b572cbfb2fb3b354acdf22e39be
Nikita Tebloev <nikita.tebloev@polyus-nt.ru>
2023-07-03 12:19:03 +0800

    fix menu

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -18,6 +18,28 @@ import drive1 from './img/flash-drive1.png';
 import chip1 from './img/chip1.png';
 import gear1 from './img/gear1.png';
 import arrow1 from './img/arrow1.png';
+import { twMerge } from 'tailwind-merge';
+
+const sidebar = [
+  {
+    imgSrc: menu,
+  },
+  {
+    imgSrc: components1,
+  },
+  {
+    imgSrc: programming,
+  },
+  {
+    imgSrc: drive1,
+  },
+  {
+    imgSrc: chip1,
+  },
+  {
+    imgSrc: gear1,
+  },
+];
 
 export const App: React.FC = () => {
   const [fileContent, setFileContent] = useState<string | null>(null);
@@ -26,8 +48,8 @@ export const App: React.FC = () => {
 
   /*Переключение блоков в меню между собой*/
   const [activeIndex, setActiveIndex] = useState(0);
-  const handleClick = (index) => setActiveIndex(index);
-  const checkActive = (index, className) => (activeIndex === index ? className : '');
+  const handleClick = (index: number) => () => setActiveIndex(index);
+  const isActive = (index: number) => activeIndex === index;
 
   /*Переключение блоков в меню между собой*/
   const [activeIndexDoc, setActiveIndexDoc] = useState(0);
@@ -54,73 +76,26 @@ export const App: React.FC = () => {
   return (
     <div className="flex flex-row">
       <section className="flex h-screen w-[4vw] flex-col items-center bg-transparent">
-        <button
-          className={`h-[4.5vw] w-[4vw] ${checkActive(1, '!bg-[#4391BF] !bg-opacity-50')}`}
-          onClick={() => (activeIndex === 1 ? handleClick(0) : handleClick(1))}
-        >
-          <img src={menu} alt="" className="m-auto h-[2.5vw] w-[2.5vw]"></img>
-        </button>
-        <hr className="h-[1px] w-[4vw] border-none bg-[#4391BF]" />
-        <button
-          className={`h-[4.5vw] w-[4vw] ${checkActive(2, '!bg-[#4391BF] !bg-opacity-50')}`}
-          onClick={() => (activeIndex === 2 ? handleClick(0) : handleClick(2), handleOpenFile())}
-        >
-          <img
-            src={activeIndex == 2 ? components1 : components}
-            alt=""
-            className="m-auto h-[2.5vw] w-[2.5vw]"
-          ></img>
-        </button>
-        <button
-          className={`h-[4.5vw] w-[4vw] ${checkActive(3, '!bg-[#4391BF] !bg-opacity-50')}`}
-          onClick={() => (
-            activeIndex === 3 ? handleClick(0) : handleClick(3), handleToggleCodeEditor()
-          )}
-        >
-          <img
-            src={activeIndex == 3 ? programming : programming1}
-            alt=""
-            className="m-auto h-[2.5vw] w-[2.5vw]"
-          ></img>
-        </button>
-        <button
-          className={`h-[4.5vw] w-[4vw] ${checkActive(4, '!bg-[#4391BF] !bg-opacity-50')}`}
-          onClick={() => (activeIndex === 4 ? handleClick(0) : handleClick(4))}
-        >
-          <img
-            src={activeIndex == 4 ? drive1 : drive}
-            alt=""
-            className="m-auto h-[2.5vw] w-[2.5vw]"
-          ></img>
-        </button>
-        <button
-          className={`h-[4.5vw] w-[4vw] ${checkActive(5, '!bg-[#4391BF] !bg-opacity-50')}`}
-          onClick={() => (activeIndex === 5 ? handleClick(0) : handleClick(5))}
-        >
-          <img
-            src={activeIndex == 5 ? chip1 : chip}
-            alt=""
-            className="m-auto h-[2.5vw] w-[2.5vw]"
-          ></img>
-        </button>
-        <button
-          className={`mt-auto h-[4.5vw] w-[4vw] ${checkActive(6, '!bg-[#4391BF] !bg-opacity-50')}`}
-          onClick={() => (activeIndex === 6 ? handleClick(0) : handleClick(6))}
-        >
-          <img
-            src={activeIndex == 6 ? gear1 : gear}
-            alt=""
-            className="m-auto h-[2.5vw] w-[2.5vw]"
-          ></img>
-        </button>
+        {sidebar.map(({ imgSrc }, index) => (
+          <button
+            className={twMerge(
+              'h-[4.5vw] w-[4vw]',
+              isActive(index) && '!bg-[#4391BF] !bg-opacity-50'
+            )}
+            onClick={handleClick(index)}
+          >
+            <img src={imgSrc} alt="" className="m-auto h-[2.5vw] w-[2.5vw]"></img>
+          </button>
+        ))}
       </section>
-      <div className={`hidden ${checkActive(1, '!block')}`}>
+
+      <div className={twMerge('hidden', isActive(1) && 'block')}>
         <Menu />
       </div>
-      <div className={`hidden ${checkActive(2, '!block')}`}>
+      <div className={twMerge('hidden', isActive(2) && 'block')}>
         <Explorer />
       </div>
-      <div className={`hidden ${checkActive(3, '!block')}`}>
+      <div className={twMerge('hidden', isActive(3) && 'block')}>
         {isCodeEditorOpen && fileContent && <CodeEditor value={fileContent} />}
       </div>
       <div className={`flex h-[95vh] w-[92vw] flex-col`}>

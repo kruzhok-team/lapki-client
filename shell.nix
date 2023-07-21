324ac88fc125b16673a0f9d4db585e82bc44fbe0
Mikhail Chekan <mikhail.chekan@polyus-nt.ru>
2023-07-21 18:10:04 +0000

    окружение разработки для NixOS
    Команда nix-shell сразу же запускает VS Code, в котором можно
    выполнить npm install, npm run dev и перейти к разработке.
    Упаковывать клиент под Nix мы пока не будем.

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+pkgs.mkShell { 
+  packages = [
+    ( pkgs.vscode.fhsWithPackages (ps: with ps; ( 
+        atomEnv.packages  
+        ++
+        [ libxkbcommon zlib openssl.dev 
+          nodejs-18_x 
+          pkg-config
+          git openssh nano
+        ]
+      ))
+    )
+  ];
+  shellHook = ''
+    code .
+  '';
+}

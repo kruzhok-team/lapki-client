d25a8aeb114219a121d7becb6422f6e3bf0159a5
XidFanSan <fedya4444baskov.ru@gmail.com>
2023-08-01 20:47:13 +0800

    Добавлен уникальный ключ для элементов проводника.

--- src/renderer/src/components/Explorer.tsx
+++ src/renderer/src/components/Explorer.tsx
@@ -1,10 +1,11 @@
 import { StateMachine } from '@renderer/lib/data/StateMachine';
 import React from 'react';
+
 interface ExplorerProps {
   stateMachine: StateMachine | undefined;
 }
 
-export const Explorer: React.FC<ExplorerProps> = ({stateMachine}) => {
+export const Explorer: React.FC<ExplorerProps> = ({ stateMachine }) => {
   console.log(stateMachine);
   return (
     <section className="flex h-full flex-col justify-between bg-[#4391BF] bg-opacity-50 font-Fira text-base">
@@ -12,14 +13,15 @@ export const Explorer: React.FC<ExplorerProps> = ({stateMachine}) => {
         <h1 className="mb-3 border-b border-white pb-2 text-lg">Проводник</h1>
 
         <h2 className="mb-4 ">Компоненты</h2>
-        {stateMachine && <div>
-          {
-            [...stateMachine.components.keys()].map((key) => 
-            <div>
+        {stateMachine && (
+          <div>
+            {[...stateMachine.components.keys()].map((key) => (
+              <div key={'explorer' + key}>
                 <img src={stateMachine.components.get(key)?.image.src}></img> {key}
-              </div>)
-          }
-          </div>}
+              </div>
+            ))}
+          </div>
+        )}
         <div className="cursor-pointer rounded bg-neutral-700 px-4 py-3 text-neutral-50" draggable>
           Состояние
         </div>

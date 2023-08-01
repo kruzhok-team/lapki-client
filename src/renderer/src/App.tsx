f959895167ba94bfae2a0da554cb4a26748fdcc3
XidFanSan <fedya4444baskov.ru@gmail.com>
2023-08-01 20:50:30 +0800

    Изменена логика поведения tabs.
    1) Компонент tabs был перенесен в компонент app и подкорректирован код;
    2) реализована частичная работа "просмотра кода для нод(связей);

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 import { Panel, PanelGroup } from 'react-resizable-panels';
 
-import { CodeEditor, DiagramEditor, Documentations, Tabs } from './components';
+import { CodeEditor, DiagramEditor, Documentations } from './components';
 import { Sidebar } from './components/Sidebar';
 import { Elements } from './types/diagram';
 /*Первые иконки*/
@@ -10,18 +10,20 @@ import arrow from './assets/img/arrow.png';
 // import forward from './assets/img/forward.png';
 /*Вторичные иконки*/
 import arrow1 from './assets/img/arrow1.png';
+import { ReactComponent as Cross } from '@renderer/assets/icons/cross.svg';
 import { CanvasEditor } from './lib/CanvasEditor';
 import { preloadPicto } from './lib/drawable/Picto';
+
 /**
  * React-компонент приложения
  */
 export const App: React.FC = () => {
-  preloadPicto(()=> void {});
+  preloadPicto(() => void {});
 
   // TODO: а если у нас будет несколько редакторов?
 
   const [editor, setEditor] = useState<CanvasEditor | null>(null);
-  const [fileName, setFileName] = useState<string | null>(null);
+  let [fileName, setFileName] = useState<string | null>(null);
   const [fileContent, setFileContent] = useState<string | null>(null);
   const elements = fileContent ? (JSON.parse(fileContent) as Elements) : null;
   const [isDocOpen, setIsDocOpen] = useState(false);
@@ -42,25 +44,55 @@ export const App: React.FC = () => {
     console.log(FileNew);
   };
 
+  //Callback данные для получения ответа от контекстного меню
+  const [idTextCode, setIdTextCode] = useState<string | null>(null);
+
   /** Callback функция выбора вкладки (машина состояний, код) */
-  const [activeTab, setActiveTab] = useState<number | 0>(0);
-  const isActive = (index: number) => activeTab === index;
+  var [activeTab, setActiveTab] = useState<number | 0>(0);
+  var isActive = (index: number) => activeTab === index;
   const handleClick = (index: number) => {
     if (activeTab === index) {
-      return setActiveTab(activeTab);
+      setActiveTab(activeTab);
     }
     setActiveTab(index);
   };
 
-  const ActiveEditor = [
-    <DiagramEditor elements={elements!} editor={editor} setEditor={setEditor}/>,
-    <CodeEditor value={localStorage.getItem('Data') ?? ''} />,
+  var TabsItems = [
+    {
+      tab: 'SM: ' + fileName,
+      content: (
+        <DiagramEditor
+          elements={elements!}
+          editor={editor}
+          setEditor={setEditor}
+          setIdTextCode={setIdTextCode}
+        />
+      ),
+    },
+    {
+      tab: 'CODE: ' + fileName,
+      content: <CodeEditor value={localStorage.getItem('Data') ?? ''} />,
+    },
   ];
+
+  TabsItems.forEach((name, id) => {
+    if (idTextCode !== null)
+      //создаем новый элемент в массиве вкладок
+      TabsItems.push({
+        tab: idTextCode,
+        content: <CodeEditor value={idTextCode ?? ''} />,
+      });
+    id + 1;
+  });
 
   return (
     <div className="h-screen select-none">
       <PanelGroup direction="horizontal">
-        <Sidebar stateMachine={editor?.container.machine} onRequestOpenFile={handleOpenFile} onRequestNewFile={handleNewFile} />
+        <Sidebar
+          stateMachine={editor?.container.machine}
+          onRequestOpenFile={handleOpenFile}
+          onRequestNewFile={handleNewFile}
+        />
 
         <Panel>
           <div className="flex">
@@ -68,18 +100,35 @@ export const App: React.FC = () => {
               {elements ? (
                 <>
                   <div className="flex h-[2rem] items-center border-b border-[#4391BF]">
-                    <Tabs fileName={fileName} functionTabs={handleClick} isActive={isActive} />
-                    <p></p>
+                    <div className="flex font-Fira">
+                      {TabsItems.map((name, id) => (
+                        <div
+                          key={'tab' + id}
+                          className={twMerge(
+                            'flex items-center',
+                            isActive(id) && 'bg-[#4391BF] bg-opacity-50'
+                          )}
+                          onClick={() => handleClick(id)}
+                        >
+                          <div role="button" className="line-clamp-1 p-1">
+                            {name.tab}
+                          </div>
+                          <button className="p-2 hover:bg-[#FFFFFF]">
+                            <Cross width="1rem" height="1rem" />
+                          </button>
+                        </div>
+                      ))}
+                    </div>
                     {/*<button className="w-[4vw]">
                       <img src={forward} alt="" className="m-auto h-[2.5vw] w-[2.5vw]"></img>
                     </button>*/}
                   </div>
-                  {ActiveEditor.map((Element, i) => (
+                  {TabsItems.map((name, id) => (
                     <div
-                      key={i + 'ActiveBlock'}
-                      className={twMerge('hidden h-[calc(100vh-2rem)]', isActive(i) && 'block')}
+                      key={id + 'ActiveBlock'}
+                      className={twMerge('hidden h-[calc(100vh-2rem)]', isActive(id) && 'block')}
                     >
-                      {Element}
+                      {name.content}
                     </div>
                   ))}
                 </>

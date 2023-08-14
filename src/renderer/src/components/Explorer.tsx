3d5ae06a87c8fd5288f419be167263c55e382eef
Mikhail Chekan <mikhail.chekan@polyus-nt.ru>
2023-08-14 17:51:24 +0000

    итерация списка компонентов

--- src/renderer/src/components/Explorer.tsx
+++ src/renderer/src/components/Explorer.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UnknownIcon from '@renderer/assets/icons/unknown.svg';
 import { EditorRef } from './utils/useEditorManager';
+import ScrollableList, { ScrollableListItem } from './utils/ScrollableList';
+import { twMerge } from 'tailwind-merge';
+import AddIcon from '@renderer/assets/icons/new transition.svg';
+
+import './component-list.css';
 
 interface ExplorerProps {
   editorRef: EditorRef;
@@ -8,24 +13,75 @@ interface ExplorerProps {
 
 export const Explorer: React.FC<ExplorerProps> = ({ editorRef }) => {
   const editorData = editorRef.editorData;
+  const [cursor, setCursor] = useState<string | null>(null);
+
+  const onUnClick = (_e: React.MouseEvent) => {
+    setCursor(null);
+  };
+
+  const onCompClick = (e: React.MouseEvent, key: string) => {
+    e.stopPropagation();
+    setCursor(key);
+  };
+
+  const onCompDblClick = (e: React.MouseEvent, key: string) => {
+    e.stopPropagation();
+    console.log(['component-double-click', key]);
+  };
+
+  const onCompRightClick = (e: React.MouseEvent, key: string) => {
+    e.stopPropagation();
+    console.log(['component-right-click', key]);
+  };
+
+  const onAddClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    console.log(['add-component-click']);
+  };
+
+  const content: ScrollableListItem[] = Object.entries(editorData?.data.components).map(
+    ([key, _component]) => ({
+      id: key,
+      content: (
+        <div
+          className={twMerge(key == cursor && 'bg-slate-600', 'flex items-center pb-1 pt-1')}
+          onClick={(e) => onCompClick(e, key)}
+          onDoubleClick={(e) => onCompDblClick(e, key)}
+          onContextMenu={(e) => onCompRightClick(e, key)}
+        >
+          <img
+            style={{ height: '32px' }}
+            src={editorRef.platform?.getComponentIconUrl(key, true) ?? UnknownIcon}
+          />
+          <p className={twMerge('line-clamp-1', key == cursor && 'text-white')}>{key}</p>
+        </div>
+      ),
+    })
+  );
+
   return (
-    <section className="flex h-full flex-col justify-between bg-[#a1c8df] font-Fira text-base">
-      <div className="w-full px-4 pt-2 text-center">
+    <section
+      className="flex h-full flex-col justify-start bg-[#a1c8df] font-Fira text-base"
+      onClick={onUnClick}
+    >
+      <div
+        className="w-full flex-auto px-4 pt-2 text-center"
+        style={{ height: `{dividerHeight}px` }}
+      >
         <h1 className="mb-3 border-b border-white pb-2 text-lg">Компоненты</h1>
-        {editorData?.content && (
-          <div className="flex flex-col items-center">
-            {Object.entries(editorData?.data.components).map(([key, _component]) => (
-              <div className="mb-4 h-20 w-20 cursor-pointer" draggable key={'explorer' + key}>
-                <img src={editorRef.platform?.getComponentIconUrl(key, true) ?? UnknownIcon} />
-                {key}
-              </div>
-            ))}
-          </div>
-        )}
-        <div className=" rounded bg-neutral-700 px-4 py-3 text-neutral-50">Добавить компонент</div>
+        <div className="component-add" onClick={onAddClick}>
+          <img src={AddIcon} />
+          <p>Добавить...</p>
+        </div>
+        <ScrollableList
+          listItems={content ?? []}
+          heightOfItem={10}
+          maxItemsToRender={50}
+          className="component-list"
+        />
       </div>
 
-      <div className="h-[500px] px-4 text-center">
+      <div className="h-full flex-auto px-4 pt-3 text-center">
         <h1 className="mb-3 border-b border-white  pb-2 text-lg">Иерархия состояний</h1>
 
         <div>

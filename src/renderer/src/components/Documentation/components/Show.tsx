0556de57dce4dfd5755e53d98565a1152f1255bf
Evgeniy <evgeniy.gerasimov@polyus-nt.com>
2023-08-23 14:33:46 +0800

    небольшой фикс в документации

--- src/renderer/src/components/Documentation/components/Show.tsx
+++ src/renderer/src/components/Documentation/components/Show.tsx
@@ -5,7 +5,9 @@ const Show = ({ html, documentLink }) => {
         <iframe className="h-full w-full" srcDoc={html}></iframe>
       ) : (
         <div className="p-4">
-          <h2 className="mb-2 text-xl font-bold">Ссылка на материал</h2>
+          <h2 className="mb-2 text-xl font-bold">
+            {documentLink ? 'Ссылка на материал' : 'Выберите раздел в содержании'}
+          </h2>
           <a href={documentLink} target="_blank" className="text-blue-500 hover:text-blue-700">
             {documentLink}
           </a>

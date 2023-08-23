88f63f76425fafd943e654ca51bb8089f224d9a7
Evgeniy <evgeniy.gerasimov@polyus-nt.com>
2023-08-23 14:30:46 +0800

    небольшой фикс стилей в документации

--- src/renderer/src/components/Documentation/components/Show.tsx
+++ src/renderer/src/components/Documentation/components/Show.tsx
@@ -4,7 +4,7 @@ const Show = ({ html, documentLink }) => {
       {html ? (
         <iframe className="h-full w-full" srcDoc={html}></iframe>
       ) : (
-        <div className="bg-gray-200 p-4">
+        <div className="p-4">
           <h2 className="mb-2 text-xl font-bold">Ссылка на материал</h2>
           <a href={documentLink} target="_blank" className="text-blue-500 hover:text-blue-700">
             {documentLink}

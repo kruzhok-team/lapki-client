c330443a42ab20038b3982b9f817f00bfdf94ac5
Evgeniy <evgeniy.gerasimov@polyus-nt.com>
2023-08-23 13:21:38 +0800

    сбрасываем таймаут долгого нажатия после движения курсора

--- src/renderer/src/lib/drawable/Draggable.ts
+++ src/renderer/src/lib/drawable/Draggable.ts
@@ -204,6 +204,10 @@ export class Draggable extends EventEmitter {
   handleMouseMove = (e: MyMouseEvent) => {
     if (!this.dragging || this.container.isPan) return;
 
+    if (Math.abs(e.dx) > 1 && Math.abs(e.dy) > 1) {
+      clearTimeout(this.mouseDownTimerId);
+    }
+
     this.bounds.x += e.dx * this.container.scale;
     this.bounds.y += e.dy * this.container.scale;
 

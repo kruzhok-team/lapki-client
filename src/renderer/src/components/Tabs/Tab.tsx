2ef6128533105b98106ec5d7e412d46e55a6e301
Nikita Tebloev <nikita.tebloev@polyus-nt.ru>
2023-08-28 14:15:13 +0800

    add visual dragover

---
+++ src/renderer/src/components/Tabs/Tab.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+import { Tab as TabType } from '@renderer/types/tabs';
+
+import { ReactComponent as EditorIcon } from '@renderer/assets/icons/editor.svg';
+import { ReactComponent as CodeIcon } from '@renderer/assets/icons/code.svg';
+import { ReactComponent as TransitionIcon } from '@renderer/assets/icons/transition.svg';
+import { ReactComponent as StateIcon } from '@renderer/assets/icons/state.svg';
+import { ReactComponent as CloseIcon } from '@renderer/assets/icons/close.svg';
+
+const TabIcon = {
+  editor: <EditorIcon />,
+  code: <CodeIcon />,
+  transition: <TransitionIcon />,
+  state: <StateIcon />,
+};
+
+interface TabProps {
+  isActive: boolean;
+  isDragging: boolean;
+  draggable: boolean;
+  type: TabType['type'];
+  name: string;
+  showName: boolean;
+  canClose: boolean;
+  onDragStart: () => void;
+  onDrop: () => void;
+  onMouseDown: () => void;
+  onClose: () => void;
+}
+
+export const Tab: React.FC<TabProps> = (props) => {
+  const {
+    draggable,
+    isDragging,
+    isActive,
+    type,
+    name,
+    showName,
+    canClose,
+    onDragStart,
+    onDrop,
+    onMouseDown,
+    onClose,
+  } = props;
+
+  const [dragOver, setDragOver] = useState(false);
+
+  const handleDragOver = (e: React.DragEvent) => {
+    e.preventDefault();
+
+    if (!draggable) {
+      e.dataTransfer.dropEffect = 'none';
+    }
+
+    if (!isDragging && draggable) setDragOver(true);
+  };
+
+  const handleDragLeave = () => {
+    if (!isDragging && draggable) setDragOver(false);
+  };
+
+  const handleDragStart = (e: React.DragEvent) => {
+    e.dataTransfer.effectAllowed = 'move';
+
+    onDragStart();
+  };
+
+  const handleDrop = () => {
+    onDrop();
+    setDragOver(false);
+  };
+
+  return (
+    <button
+      draggable={draggable}
+      onDragLeave={handleDragLeave}
+      onDragOver={handleDragOver}
+      onDragStart={handleDragStart}
+      onDrop={handleDrop}
+      className={twMerge(
+        'group flex cursor-pointer items-center rounded p-1 px-2 transition hover:bg-bg-primary',
+        isActive && 'bg-bg-primary',
+        dragOver && 'bg-bg-primary'
+      )}
+      onMouseDown={onMouseDown}
+    >
+      {TabIcon[type]}
+
+      {showName && (
+        <span title={name} className="ml-1 line-clamp-1 w-20 text-left">
+          {name}
+        </span>
+      )}
+
+      {canClose && (
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            onClose();
+          }}
+          className={twMerge(
+            'hover:bg-bg-btn rounded-md p-1 opacity-0 transition-opacity group-hover:opacity-100',
+            isActive && 'opacity-100'
+          )}
+        >
+          <CloseIcon className="h-3 w-3" />
+        </button>
+      )}
+    </button>
+  );
+};
---
+++ src/renderer/src/components/Tabs/Tabs.tsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+import { CodeEditor, DiagramEditor, DiagramEditorProps } from '@renderer/components';
+import { Tab as TabType } from '@renderer/types/tabs';
+
+import { Tab } from './Tab';
+
+export interface TabsProps {
+  editorProps: DiagramEditorProps;
+  items: TabType[];
+  activeTab: string;
+  setActiveTab: (tab: string) => void;
+  onClose: (tabName: string) => void;
+  onSwapTabs: (a: string, b: string) => void;
+}
+
+export const Tabs: React.FC<TabsProps> = ({
+  editorProps,
+  items,
+  activeTab,
+  setActiveTab,
+  onClose,
+  onSwapTabs,
+}: TabsProps) => {
+  const [dragId, setDragId] = useState<string | null>(null);
+
+  const handleDrag = (tabName: string) => {
+    setDragId(tabName);
+  };
+
+  const handleDrop = (tabName: string) => {
+    if (tabName === 'editor' || !dragId) return;
+
+    onSwapTabs(dragId, tabName);
+  };
+
+  return (
+    <>
+      <section className="flex gap-1 overflow-x-auto break-words border-b border-border-primary bg-bg-secondary px-1 py-1 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-[#a1c8df]">
+        {items.map(({ type, name }) => (
+          <Tab
+            isActive={activeTab === name}
+            isDragging={dragId === name}
+            draggable={type !== 'editor'}
+            type={type}
+            name={name}
+            showName={type !== 'editor'}
+            canClose={type !== 'editor'}
+            onDragStart={() => handleDrag(name)}
+            onDrop={() => handleDrop(name)}
+            onMouseDown={() => setActiveTab(name)}
+            onClose={() => onClose(name)}
+          />
+        ))}
+      </section>
+
+      {items.map((item) => (
+        <div
+          key={item.name}
+          className={twMerge('hidden h-[calc(100vh-2rem)]', activeTab === item.name && 'block')}
+        >
+          {item.type === 'editor' ? (
+            <DiagramEditor {...editorProps} />
+          ) : (
+            <CodeEditor language={item.language} value={item.code} />
+          )}
+        </div>
+      ))}
+    </>
+  );
+};

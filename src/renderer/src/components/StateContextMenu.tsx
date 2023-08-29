7aa151d1e0d58d9445d3b4ea465501b809b0edf1
Nikita Tebloev <nikita.tebloev@polyus-nt.ru>
2023-08-29 18:45:29 +0800

    fix context menu

--- src/renderer/src/components/StateContextMenu.tsx
+++ src/renderer/src/components/StateContextMenu.tsx
@@ -1,10 +1,12 @@
+import { useEffect } from 'react';
 import { useFloating, offset, flip, shift } from '@floating-ui/react';
+import { useForm } from 'react-hook-form';
+import { twMerge } from 'tailwind-merge';
+
 import { Condition } from '@renderer/lib/drawable/Condition';
 import { EventSelection } from '@renderer/lib/drawable/Events';
 import { State } from '@renderer/lib/drawable/State';
 import { Point } from '@renderer/types/graphics';
-import { useForm } from 'react-hook-form';
-import { twMerge } from 'tailwind-merge';
 
 export interface StateContextMenuData {
   data: State | Condition | null;
@@ -167,6 +169,24 @@ export const StateContextMenu: React.FC<StateContextMenuProps> = ({
       style: !isData?.data && 'hidden',
     },
   ];
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (!isOpen) return;
+
+      const ref = refs.floating;
+
+      if (ref.current && !ref.current.contains(e.target as HTMLElement)) {
+        onCloseMe();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, refs.floating, onCloseMe]);
 
   return (
     <>

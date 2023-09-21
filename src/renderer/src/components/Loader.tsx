5c5491b80769fdb5741600143e3a3c85a1e0741d
Roundabout1 <mrsivannap@gmail.com>
2023-09-21 12:41:00 +0800

    merge

--- src/renderer/src/components/Loader.tsx
+++ src/renderer/src/components/Loader.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ReactComponent as Update } from '@renderer/assets/icons/update.svg';
 import { ReactComponent as Setting } from '@renderer/assets/icons/settings.svg';
 import { twMerge } from 'tailwind-merge';
@@ -8,41 +8,165 @@ import {
   FLASHER_CONNECTED,
   FLASHER_CONNECTING,
   FLASHER_SWITCHING_HOST,
-  FLASHER_NO_CONNECTION,
   FLASHER_CONNECTION_ERROR,
+  FLASHER_NO_CONNECTION,
+  Flasher,
 } from './Modules/Flasher';
+import { EditorManager } from '@renderer/lib/data/EditorManager';
+import { FlasherSelectModal } from './FlasherSelectModal';
 export interface FlasherProps {
-  devices: Map<string, Device>;
-  currentDevice: string | undefined;
-  connectionStatus: string;
-  flasherLog: string | undefined;
+  manager: EditorManager | null;
   compilerData: CompilerResult | undefined;
-  flasherFile: string | undefined | null;
-  flashing: boolean;
-  setCurrentDevice: Dispatch<string | undefined>;
-  handleGetList: () => void;
-  handleFlash: () => void;
-  handleHostChange: () => void;
-  handleFileChoose: () => void;
-  handleErrorMessageDisplay: () => void;
 }
 
-export const Loader: React.FC<FlasherProps> = ({
-  currentDevice,
-  devices,
-  connectionStatus,
-  compilerData,
-  flasherFile,
-  flasherLog,
-  flashing,
-  setCurrentDevice,
-  handleGetList,
-  handleFlash,
-  handleHostChange,
-  handleFileChoose,
-  handleErrorMessageDisplay: handleErrorMessageDisplay,
-}) => {
+export const Loader: React.FC<FlasherProps> = ({ manager, compilerData }) => {
+  const [currentDevice, setCurrentDevice] = useState<string | undefined>(undefined);
+  const [connectionStatus, setFlasherConnectionStatus] = useState<string>('Не подключен.');
+  const [devices, setFlasherDevices] = useState<Map<string, Device>>(new Map());
+  const [flasherLog, setFlasherLog] = useState<string | undefined>(undefined);
+  const [flasherFile, setFlasherFile] = useState<string | undefined | null>(undefined);
+  const [flashing, setFlashing] = useState(false);
+  const [flasherError, setFlasherError] = useState<string | undefined>(undefined);
+  const [flasherIsLocal, setFlasherIslocal] = useState<boolean>(true);
+
+  const [isFlasherModalOpen, setIsFlasherModalOpen] = useState(false);
+  const openFlasherModal = () => setIsFlasherModalOpen(true);
+  const closeFlasherModal = () => {
+    Flasher.freezeReconnectionTimer(false);
+    setIsFlasherModalOpen(false);
+  };
+
   const isActive = (id: string) => currentDevice === id;
+
+  const handleGetList = async () => {
+    manager?.getList();
+  };
+
+  const handleFlash = async () => {
+    if (flasherFile) {
+      Flasher.flash(currentDevice!);
+    } else {
+      Flasher.flashCompiler(compilerData!.binary!, currentDevice!);
+    }
+  };
+
+  const handleHostChange = () => {
+    Flasher.freezeReconnectionTimer(true);
+    openFlasherModal();
+  };
+
+  const handleLocalFlasher = async () => {
+    console.log('local');
+    setFlasherIslocal(true);
+    await manager?.startLocalModule('lapki-flasher');
+    //Стандартный порт
+    manager?.changeFlasherLocal();
+  };
+
+  const handleRemoteFlasher = (host: string, port: number) => {
+    setFlasherIslocal(false);
+    console.log('remote');
+    // await manager?.stopLocalModule('lapki-flasher');
+    manager?.changeFlasherHost(host, port);
+  };
+
+  const handleFileChoose = () => {
+    if (flasherFile) {
+      console.log('cancel file choose');
+      setFlasherFile(undefined);
+    } else {
+      console.log('file chooser');
+      Flasher.setFile();
+    }
+  };
+
+  const handleErrorMessageDisplay = async () => {
+    // выводимое для пользователя сообщение
+    var errorMsg: JSX.Element = <p>`Неизвестный тип ошибки`</p>;
+    if (flasherIsLocal) {
+      await window.electron.ipcRenderer
+        .invoke('Module:getStatus', 'lapki-flasher')
+        .then(function (obj) {
+          let errorDetails = obj.details;
+          switch (obj.code) {
+            // код 0 означает, что не было попытки запустить загрузчик, по-идее такая ошибка не может возникнуть, если только нет какой-то ошибки в коде.
+            case 0:
+              errorMsg = <p>{'Загрузчик не был запущен по неизвестной причине.'}</p>;
+              break;
+            // код 1 означает, что загрузчик работает, но соединение с ним не установлено.
+            case 1:
+              switch (connectionStatus) {
+                case FLASHER_CONNECTION_ERROR:
+                  errorMsg = (
+                    <p>
+                      {`Локальный загрузчик работает, но он не может подключиться к IDE из-за ошибки.`}
+                      <br></br>
+                      {flasherError}
+                    </p>
+                  );
+                  break;
+                default:
+                  errorMsg = (
+                    <p>
+                      {`Локальный загрузчик работает, но IDE не может установить с ним соединение.`}
+                    </p>
+                  );
+                  break;
+              }
+              break;
+            case 2:
+              errorMsg = (
+                <p>
+                  {`Локальный загрузчик не смог запуститься из-за ошибки.`}
+                  <br></br>
+                  {errorDetails}
+                </p>
+              );
+              break;
+            case 3:
+              errorMsg = <p>{`Прервана работа локального загрузчика.`}</p>;
+              break;
+            case 4:
+              errorMsg = <p>{`Платформа ${errorDetails} не поддерживается.`}</p>;
+              break;
+          }
+        });
+    } else {
+      if (connectionStatus == FLASHER_CONNECTION_ERROR) {
+        errorMsg = (
+          <p>
+            {`Ошибка соединения.`}
+            <br></br>
+            {flasherError}
+          </p>
+        );
+      } else {
+        errorMsg = <p>{flasherError}</p>;
+      }
+    }
+    const msg: MessageModalData = {
+      text: errorMsg,
+      caption: 'Ошибка',
+    };
+    openMsgModal(msg);
+  };
+
+  useEffect(() => {
+    Flasher.bindReact(
+      setFlasherDevices,
+      setFlasherConnectionStatus,
+      setFlasherLog,
+      setFlasherFile,
+      setFlashing,
+      setFlasherError,
+      setFlasherIslocal
+    );
+    const reader = new FileReader();
+    Flasher.initReader(reader);
+    console.log('CONNECTING TO FLASHER');
+    Flasher.connect();
+    // если не указывать второй аргумент '[]', то эта функция будет постоянно вызываться.
+  }, []);
 
   return (
     <section className="flex h-full flex-col text-center">

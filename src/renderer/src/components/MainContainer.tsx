b2e6bd7d95a2b70cfb0d1900cef90ac703ac0448
XidFanSan <fedya4444baskov.ru@gmail.com>
2023-10-09 09:55:41 +0300

    Редизайн начального экрана.

--- src/renderer/src/components/MainContainer.tsx
+++ src/renderer/src/components/MainContainer.tsx
@@ -9,6 +9,8 @@ import { EditorManager } from '@renderer/lib/data/EditorManager';
 import { Documentations } from './Documentation/Documentation';
 import { Tabs } from './Tabs';
 
+import { ReactComponent as Icon } from '@renderer/assets/icons/icon.svg';
+
 interface MainContainerProps {
   manager: EditorManager;
   editor: CanvasEditor | null;
@@ -41,6 +43,43 @@ export const MainContainer: React.FC<MainContainerProps> = ({
     onDrop,
   });
 
+  const combination = [
+    {
+      name: "Создать файл ",
+      command: {
+        button1: "Ctrl",
+        button2: "N",
+      }
+    },
+    {
+      name: "Открыть файл ",
+      command: {
+        button1: "Ctrl",
+        button2: "O",
+      }
+    },
+    {
+      name: "Обновить приложение ",
+      command: {
+        button1: "Ctrl",
+        button2: "R",
+      }
+    },
+    {
+      name: "Во весь экран ",
+      command: {
+        button1: "F11",
+        button2: undefined,
+      }
+    },
+    {
+      name: "DevTools ",
+      command: {
+        button1: "F12",
+        button2: undefined,
+      }
+    }
+  ]
   return (
     <div
       className={twMerge(
@@ -55,9 +94,29 @@ export const MainContainer: React.FC<MainContainerProps> = ({
       {isInitialized ? (
         <Tabs manager={manager} editor={editor} setEditor={setEditor} />
       ) : (
-        <p className="pt-24 text-center text-base">
-          Откройте файл в формате json или перенесите его сюда...
-        </p>
+        <div className="flex flex-col items-center pt-24">
+          <Icon />
+          <p className="text-center text-base py-6">
+            Перетащите файл в эту область, либо воспользуйтесь комбинациями клавиш:
+          </p>
+          <div> 
+          
+            {
+              
+              combination.map((value) => (
+                <div className="flex justify-between my-3">
+                  <div className="px-1">{value.name}</div>
+                  <div className="flex items-start">
+                    <div className="bg-gray-600 px-1 rounded border-b-2 text-gray-300">{value.command.button1}</div>
+                    {
+                      value.command.button2 ? <><p className="px-1">+</p><div className="bg-gray-600 border-b-2 px-1 rounded text-gray-300">{value.command.button2}</div></> : ''
+                    }
+                  </div>
+                  </div>
+              ))
+            }
+          </div>
+        </div>
       )}
 
       <Documentations topOffset={!!isInitialized} baseUrl={'https://lapki-doc.polyus-nt.ru/'} />

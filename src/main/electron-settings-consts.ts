2c18c53e5164b98d6c34626e84670b939c28d46d
Roundabout1 <mrsivannap@gmail.com>
2023-10-30 15:04:44 +0800

    Новый файл для хранения констант для electron-settings

---
+++ src/main/electron-settings-consts.ts
@@ -0,0 +1,11 @@
+// константы для использования electron-settings
+
+// стандартные значения для хоста и порта загрузчика
+
+export const DEFAULT_COMPILER_HOST: string = 'lapki.polyus-nt.ru';
+export const DEFAULT_COMPILER_PORT: number = 8081;
+
+// названия ключей electron-settings
+
+export const COMPILER_SETTINGS: string = 'compiler';
+export const PLATFORMS_PATH_SETTINGS: string = 'PlatformsPath';

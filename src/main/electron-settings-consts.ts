22c0ce161a378a3773a46ecba76b01e3f2bafcb0
Roundabout1 <mrsivannap@gmail.com>
2023-10-31 14:36:02 +0800

    Рефакторинг констант

--- src/main/electron-settings-consts.ts
+++ src/main/electron-settings-consts.ts
@@ -2,29 +2,13 @@
 
 // стандартные значения для хоста и порта загрузчика
 
-export const DEFAULT_COMPILER_HOST: string = 'lapki.polyus-nt.ru';
-export const DEFAULT_COMPILER_PORT: number = 8081;
+export const DEFAULT_COMPILER_SETTINGS = {
+  host: 'lapki.polyus-nt.ru',
+  port: 8081,
+};
 
 // названия ключей electron-settings
 
-export interface FlasherSettingsKeys {
-  key: string;
-  params: {
-    remoteHost: string;
-    remotePort: string;
-    localHost: string;
-    localPort: string;
-  };
-}
-
-export const COMPILER_SETTINGS: string = 'compiler';
-export const FLASHER_SETTINGS: FlasherSettingsKeys = {
-  key: 'flasher',
-  params: {
-    remoteHost: 'remoteHost',
-    remotePort: 'remotePort',
-    localHost: 'localHost',
-    localPort: 'localPort',
-  },
-};
-export const PLATFORMS_PATH_SETTINGS: string = 'PlatformsPath';
+export const COMPILER_SETTINGS_KEY: string = 'compiler';
+export const FLASHER_SETTINGS_KEY: string = 'flasher';
+export const PLATFORMS_PATH_SETTINGS_KEY: string = 'PlatformsPath';

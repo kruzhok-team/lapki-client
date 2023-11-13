e3c9900d3ec0ce6c707e7142d4291953141f9b77
bryzZz <tebloev.nikita@mail.ru>
2023-11-13 11:59:50 +0800

    change buttons to list

--- src/renderer/src/components/PlatformSelectModal.tsx
+++ src/renderer/src/components/PlatformSelectModal.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+
+import { twMerge } from 'tailwind-merge';
 
 import { getAvailablePlatforms } from '@renderer/lib/data/PlatformLoader';
 
@@ -15,26 +17,56 @@ export const PlatformSelectModal: React.FC<PlatformSelectModalProps> = ({
   onCreate,
   ...props
 }) => {
-  const handleClick = (idx: string) => {
-    onCreate(idx);
-    onClose();
+  const [selectedPlatformIdx, setSelectedPlatformIdx] = useState<string | null>(null);
+
+  const handleClick = (idx: string) => () => setSelectedPlatformIdx(idx);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!selectedPlatformIdx) return;
+
+    onCreate(selectedPlatformIdx);
+    handleCLose();
   };
 
+  const handleCLose = () => {
+    onClose();
+    setSelectedPlatformIdx(null);
+  };
+
+  const isSelected = (idx: string) => selectedPlatformIdx === idx;
+
   const platforms = getAvailablePlatforms();
+  const selectedPlatform = useMemo(
+    () => platforms.find(({ idx }) => selectedPlatformIdx === idx),
+    [platforms, selectedPlatformIdx]
+  );
 
   return (
-    <Modal {...props} onRequestClose={onClose} title="Выберите платформу">
-      <div className="flex flex-col items-start gap-2">
-        {platforms.map((platform) => (
-          <button
-            key={platform.idx}
-            type="button"
-            className="rounded bg-neutral-700 px-4 py-2 transition-colors hover:bg-neutral-600"
-            onClick={() => handleClick(platform.idx)}
-          >
-            {platform.name}
-          </button>
-        ))}
+    <Modal
+      {...props}
+      onRequestClose={handleCLose}
+      onSubmit={handleSubmit}
+      title="Выберите платформу"
+    >
+      <div className="grid grid-cols-2 gap-4">
+        <div className="max-h-[40vh] w-full overflow-y-auto scrollbar-thin scrollbar-track-scrollbar-track scrollbar-thumb-scrollbar-thumb">
+          {platforms.map(({ idx, name }) => (
+            <div
+              key={idx}
+              className={twMerge(
+                'flex cursor-pointer select-none items-center gap-2 p-2 transition-colors duration-75',
+                isSelected(idx) && 'bg-bg-active'
+              )}
+              onClick={handleClick(idx)}
+            >
+              {name}
+            </div>
+          ))}
+        </div>
+
+        <div>{selectedPlatform?.description || 'Пока что нет описания'}</div>
       </div>
     </Modal>
   );

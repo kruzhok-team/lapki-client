07743a6a24d6ea43dad1df05f5295cd175ac088c
XidFanSan <fedya4444baskov.ru@gmail.com>
2023-11-24 17:30:38 +0800

    Создание кнопки "О программе"

--- src/renderer/src/components/Sidebar/Setting.tsx
+++ src/renderer/src/components/Sidebar/Setting.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Select } from '@renderer/components/UI';
 import { useThemeContext } from '@renderer/store/ThemeContext';
+
+import { AboutTheProgramModal } from '../AboutTheProgramModal';
 
 interface SettingProps {}
 
@@ -19,14 +21,20 @@ const themeOptions = [
 export const Setting: React.FC<SettingProps> = () => {
   const { setTheme, theme } = useThemeContext();
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => setIsModalOpen(false);
+
   return (
     <section className="flex flex-col">
       <h3 className="mx-4 mb-3 border-b border-border-primary py-2 text-center text-lg">
         Настройки
       </h3>
 
-      <div className="px-4">
-        <div>
+      <div className="flex flex-col px-4">
+        <div className="mb-4">
           Тема
           <Select
             options={themeOptions}
@@ -35,7 +43,11 @@ export const Setting: React.FC<SettingProps> = () => {
             isSearchable={false}
           />
         </div>
+        <button className="btn-primary mb-2" onClick={() => setIsModalOpen(true)}>
+          О программе
+        </button>
       </div>
+      <AboutTheProgramModal isOpen={isModalOpen} onClose={closeModal} />
     </section>
   );
 };

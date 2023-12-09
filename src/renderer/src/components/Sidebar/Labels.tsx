7a6ddf9c733748a4a48bb8b51a6790330d2305ab
Никита Теблоев <tebloev.nikita@gmail.com>
2023-12-09 14:16:07 +0800

    Подсказки и изменение иконок в сайдбаре (#134)

--- src/renderer/src/components/Sidebar/Labels.tsx
+++ src/renderer/src/components/Sidebar/Labels.tsx
@@ -1,9 +1,13 @@
-import { useSidebar } from '@renderer/store/useSidebar';
 import React from 'react';
+
 import { twMerge } from 'tailwind-merge';
 
+import { useSidebar } from '@renderer/store/useSidebar';
+
+import { WithHint } from '../WithHint';
+
 interface LabelsProps {
-  items: Array<{ Icon: JSX.Element; bottom?: boolean }>;
+  items: Array<{ Icon: JSX.Element; bottom?: boolean; hint: string }>;
 }
 
 export const Labels: React.FC<LabelsProps> = ({ items }) => {
@@ -11,18 +15,23 @@ export const Labels: React.FC<LabelsProps> = ({ items }) => {
 
   return (
     <div className="flex flex-col border-r border-border-primary bg-bg-primary">
-      {items.map(({ Icon, bottom = false }, i) => (
-        <button
-          key={i}
-          className={twMerge(
-            'w-12 border-l-2 border-transparent p-2 text-text-inactive transition-colors hover:text-text-primary',
-            activeTab === i && 'border-primary text-text-primary',
-            bottom && 'mt-auto'
+      {items.map(({ Icon, bottom = false, hint }, i) => (
+        <WithHint hint={hint} placement="right" offset={5} delay={100}>
+          {(props) => (
+            <button
+              key={i}
+              className={twMerge(
+                'w-12 border-l-2 border-transparent p-2 text-text-inactive transition-colors hover:text-text-primary',
+                activeTab === i && 'border-primary text-text-primary',
+                bottom && 'mt-auto'
+              )}
+              onClick={() => changeTab(i)}
+              {...props}
+            >
+              {Icon}
+            </button>
           )}
-          onClick={() => changeTab(i)}
-        >
-          {Icon}
-        </button>
+        </WithHint>
       ))}
     </div>
   );

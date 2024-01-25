d42eec7461196c9afc186ebb456375099c5877e1
Mikhail Chekan <chekoopa@mail.ru>
2024-01-25 05:31:32 +0000

    Добавлено скрытие платформ
    Пока не готовых 🤫

--- src/renderer/src/lib/codecs/ElementsJSONCodec.ts
+++ src/renderer/src/lib/codecs/ElementsJSONCodec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-empty */
 /**
  * Кодер JSON-формата схемы.
  * Выполняет кодирование и декодирование с проверкой данных на корректность.

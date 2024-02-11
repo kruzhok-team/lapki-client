11d59982d2128a0615bb2bebc6b55861859d0056
Roundabout1 <mrsivannap@gmail.com>
2024-02-11 21:20:23 +0800

    удаление useEffect

--- src/renderer/src/components/FilePropertiesModal.tsx
+++ src/renderer/src/components/FilePropertiesModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { Modal } from '@renderer/components/UI';
 import { EditorManager } from '@renderer/lib/data/EditorManager';

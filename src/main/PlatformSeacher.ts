59f2e9a39d728b83dbe3d086d278dd9375a4a850
Roundabout1 <86153878+Roundabout1@users.noreply.github.com>
2024-02-12 13:33:16 +0800

    Окно «Свойства» (#219)

--- src/main/PlatformSeacher.ts
+++ src/main/PlatformSeacher.ts
@@ -1,20 +1,20 @@
 import { app } from 'electron';
-import path from 'path';
 import settings from 'electron-settings';
+
+import path from 'path';
+
 import { handleGetPlatforms } from './file-handlers';
 
 const basePath = path.join(__dirname, '../../resources').replace('app.asar', 'app.asar.unpacked');
 
 const DEFAULT_PATH = [basePath + '/platform', app.getPath('userData') + '/platform'];
 
-
-
 export async function searchPlatforms() {
   return new Promise(async (resolve, _reject) => {
     const platformsPaths = new Array<string>();
     const userPath: any = await settings.get('PlatformsPath');
-    let platformFound = false
-    if (userPath.path != "") {
+    let platformFound = false;
+    if (userPath.path != '') {
       DEFAULT_PATH.push(userPath!.path);
     }
     for (const path of DEFAULT_PATH) {
@@ -22,7 +22,7 @@ export async function searchPlatforms() {
       if (response[0]) {
         if (response[1].length > 0) {
           platformsPaths.push(...response[1]);
-          platformFound = true
+          platformFound = true;
         }
       }
     }

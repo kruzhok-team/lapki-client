24431f523630d4f9c774cd8ebf5b51752681be6d
XidFanSan <fedya4444baskov.ru@gmail.com>
2024-04-23 17:31:45 +0800

    Редизайн блока документации.

--- src/renderer/src/store/useDoc.ts
+++ src/renderer/src/store/useDoc.ts
@@ -3,9 +3,13 @@ import { create } from 'zustand';
 interface DocState {
   isOpen: boolean;
   toggle: () => void;
+  isCollapsed: boolean;
+  setIsCollapsed: (value: boolean) => void;
 }
 
 export const useDoc = create<DocState>((set) => ({
   isOpen: false,
   toggle: () => set(({ isOpen }) => ({ isOpen: !isOpen })),
+  isCollapsed: false,
+  setIsCollapsed: (isCollapsed) => set({ isCollapsed }),
 }));

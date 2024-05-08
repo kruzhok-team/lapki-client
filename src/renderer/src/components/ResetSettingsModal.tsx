dd8c4fc2fe6ed8c77214bf96c8f2b50769db2362
Roundabout1 <mrsivannap@gmail.com>
2024-05-08 17:12:03 +0800

    Модальное окно сброса настроек

---
+++ src/renderer/src/components/ResetSettingsModal.tsx
@@ -0,0 +1,34 @@
+import { useForm } from 'react-hook-form';
+
+import { Modal } from '@renderer/components/UI';
+import { useSettings } from '@renderer/hooks';
+
+interface ResetSettingsModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+/*
+Окно, ожидающее подтвержение пользователя о том, что он хочет сбросить настройки
+*/
+export const ResetSettingsModal: React.FC<ResetSettingsModalProps> = ({ onClose, ...props }) => {
+  const { handleSubmit: hookHandleSubmit } = useForm();
+  const RESET_LABEL = 'Сбросить настройки';
+  const handleSubmit = hookHandleSubmit(() => {
+    //TODO: реализовать функционал
+    console.log('СБРОС НАСТРОЕК');
+    onClose();
+  });
+
+  return (
+    <Modal
+      {...props}
+      onRequestClose={onClose}
+      title="Сброс настроек"
+      submitLabel={RESET_LABEL}
+      onSubmit={handleSubmit}
+    >
+      Вы уверены, что хотите сбросить настройки? Это действие нельзя будет отменить. После нажатия
+      на кнопку "{RESET_LABEL}" IDE перезапуститься, а все значения настроек вернутся к изначальным.
+    </Modal>
+  );
+};

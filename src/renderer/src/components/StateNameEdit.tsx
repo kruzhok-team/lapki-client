dcc073763b3dd5084d37c13f920ff770bd4cc2d0
Никита Теблоев <tebloev.nikita@gmail.com>
2024-06-07 14:56:44 +0800

    Рефакторинг переименования состояния (#326)

---
+++ src/renderer/src/components/StateNameEdit.tsx
@@ -0,0 +1,121 @@
+import React, {
+  useEffect,
+  useState,
+  CSSProperties,
+  useRef,
+  useLayoutEffect,
+  useCallback,
+} from 'react';
+
+import { twMerge } from 'tailwind-merge';
+
+import { WithHint } from '@renderer/components/UI';
+import { useModal } from '@renderer/hooks';
+import { State } from '@renderer/lib/drawable';
+import { useEditorContext } from '@renderer/store/EditorContext';
+
+export const StateNameEdit: React.FC = () => {
+  const editor = useEditorContext();
+
+  const [isOpen, open, close] = useModal(false);
+  const [stateId, setStateId] = useState<string | null>(null);
+  const [initialName, setInitialName] = useState<string | null>(null);
+  const [style, setStyle] = useState({} as CSSProperties);
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = useCallback(() => {
+    const el = ref.current;
+    const value = (el?.value ?? '').trim();
+
+    if (stateId !== null && initialName !== value)
+      editor.controller.states.changeStateName(stateId, value);
+
+    setStateId(null);
+    setInitialName(null);
+    close();
+  }, [close, editor.controller.states, initialName, stateId]);
+
+  const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') return handleSubmit();
+    // Отмена редактирования
+    if (e.key === 'Escape') {
+      setStateId(null);
+      setInitialName(null);
+      return close();
+    }
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener('wheel', handleSubmit);
+    return () => window.removeEventListener('wheel', handleSubmit);
+  }, [handleSubmit]);
+
+  useEffect(() => {
+    if (!editor) return;
+
+    const handler = (state: State) => {
+      const el = ref.current;
+      if (!el) return;
+
+      const globalOffset = editor.view.app.mouse.getOffset();
+      const statePos = state.computedPosition;
+      const position = {
+        x: statePos.x + globalOffset.x,
+        y: statePos.y + globalOffset.y,
+      };
+      const sizes = state.computedTitleSizes;
+
+      setStateId(state.id);
+      setInitialName(state.data.name);
+      setStyle({
+        left: position.x + 'px',
+        top: position.y + 'px',
+        width: sizes.width + 'px',
+        height: sizes.height + 'px',
+        fontSize: Math.max(sizes.fontSize, 15) + 'px',
+        padding: `${0}px ${Math.max(sizes.paddingX, 15)}px`,
+      });
+
+      el.value = state.data.name;
+      setTimeout(() => el.focus(), 0);
+      open();
+    };
+
+    editor.controller.states.on('changeStateName', handler);
+
+    return () => {
+      editor.controller.states.off('changeStateName', handler);
+    };
+  }, [editor, open]);
+
+  return (
+    <WithHint
+      hint="Нажмите ⏎, чтобы применить"
+      offset={{ crossAxis: -20, mainAxis: 10 }}
+      placement="top-end"
+    >
+      {(props) => (
+        <input
+          {...props}
+          ref={ref}
+          style={style}
+          className={twMerge(
+            'fixed rounded-t-[6px] bg-[#525252] text-white outline outline-2 outline-white',
+            !isOpen && 'hidden'
+          )}
+          placeholder="Придумайте название"
+          maxLength={50}
+          onKeyUp={handleKeyUp}
+          onBlur={handleSubmit}
+          // Выставление каретки в конец
+          onFocus={(e) =>
+            e.currentTarget.setSelectionRange(
+              e.currentTarget.value.length,
+              e.currentTarget.value.length
+            )
+          }
+        />
+      )}
+    </WithHint>
+  );
+};

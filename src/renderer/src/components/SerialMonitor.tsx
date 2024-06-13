681127f013cf11ceabc3dd87b8008ea3270722c2
XidFanSan <fedya4444baskov.ru@gmail.com>
2024-06-13 19:27:04 +0800

    Новинки: 1) Добавлена кнопка "Очистить" для быстрого очищения окна сообщений; 2) Реализована смена последовательного порта.

--- src/renderer/src/components/SerialMonitor.tsx
+++ src/renderer/src/components/SerialMonitor.tsx
@@ -1,13 +1,15 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { SerialMonitor } from '@renderer/components/Modules/SerialMonitor';
 import { useSerialMonitor } from '@renderer/store/useSerialMonitor';
 
-import { Select, Switch, TextInput } from './UI';
+import { Select, SelectOption, Switch, TextInput } from './UI';
 
 export const SerialMonitorTab: React.FC = () => {
   const { autoScroll, setAutoScroll, inputValue, setInputValue, messages, setMessages } =
     useSerialMonitor();
+  const [port, setPort] = useState<SelectOption | null>({ label: 'COM1', value: 'COM1' });
+  const [baudRate, setBaudRate] = useState<SelectOption | null>({ label: '9600', value: '9600' });
 
   const messageContainerRef = useRef<HTMLDivElement>(null);
   console.log(messages);
@@ -33,12 +35,22 @@ export const SerialMonitorTab: React.FC = () => {
     scrollToBottom();
   }, [messages, autoScroll]);
 
+  useEffect(() => {
+    if (SerialMonitor.ws && SerialMonitor.ws.readyState === WebSocket.OPEN && port && baudRate) {
+      SerialMonitor.ws.send(JSON.stringify({ port: port.value, baudRate: baudRate.value }));
+    }
+  }, [port, baudRate]);
+
   const handleSend = () => {
     if (inputValue.trim()) {
       // Отправляем сообщение через SerialMonitor
-      SerialMonitor.send(inputValue);
+      SerialMonitor.send(JSON.stringify({ command: inputValue }));
       setInputValue('');
     }
+  };
+
+  const handleClear = () => {
+    setMessages(() => []);
   };
 
   return (
@@ -54,26 +66,24 @@ export const SerialMonitorTab: React.FC = () => {
           Отправить
         </button>
       </div>
-      <div
-        className="mx-2 h-full overflow-y-auto bg-bg-primary scrollbar-thin scrollbar-track-scrollbar-track scrollbar-thumb-scrollbar-thumb"
-        ref={messageContainerRef}
-      >
-        {messages.map((msg, index) => (
-          <div key={index}>{msg}</div>
-        ))}
-      </div>
       <div className="m-2 flex justify-between">
         <div className="flex w-40 items-center justify-between">
           <Switch checked={autoScroll} onCheckedChange={() => setAutoScroll(!autoScroll)} />
           Автопрокрутка
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row items-center">
           <div className="mr-2 w-48">
             <Select isSearchable={false} />
           </div>
           <div className="mr-2 w-48">
             <Select
               isSearchable={false}
+              value={port}
+              onChange={(option) => {
+                if (option) {
+                  setPort(option as SelectOption);
+                }
+              }}
               options={[
                 { label: 'COM1', value: 'COM1' },
                 { label: 'COM2', value: 'COM2' },
@@ -81,12 +91,21 @@ export const SerialMonitorTab: React.FC = () => {
                 { label: 'COM4', value: 'COM4' },
                 { label: 'COM5', value: 'COM5' },
                 { label: 'COM6', value: 'COM6' },
+                { label: 'COM7', value: 'COM7' },
+                { label: 'COM8', value: 'COM8' },
+                { label: 'COM9', value: 'COM9' },
               ]}
             />
           </div>
-          <div className="w-48">
+          <div className="mr-2 w-48">
             <Select
               isSearchable={false}
+              value={baudRate}
+              onChange={(option) => {
+                if (option) {
+                  setBaudRate(option as SelectOption);
+                }
+              }}
               options={[
                 { label: '9600', value: '9600' },
                 { label: '19200', value: '19200' },
@@ -96,7 +115,20 @@ export const SerialMonitorTab: React.FC = () => {
               ]}
             />
           </div>
+          <div>
+            <button className="btn-primary" onClick={handleClear}>
+              Очистить
+            </button>
+          </div>
         </div>
+      </div>
+      <div
+        className="mx-2 h-full overflow-y-auto bg-bg-primary scrollbar-thin scrollbar-track-scrollbar-track scrollbar-thumb-scrollbar-thumb"
+        ref={messageContainerRef}
+      >
+        {messages.map((msg, index) => (
+          <div key={index}>{msg}</div>
+        ))}
       </div>
     </section>
   );

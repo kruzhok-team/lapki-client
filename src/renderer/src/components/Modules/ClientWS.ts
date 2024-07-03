e6753b2bd88feb2074f2e8207dabaf7b9a5e8345
Roundabout1 <mrsivannap@gmail.com>
2024-07-03 15:34:09 +0800

    Пробная версия общего клиента

--- src/renderer/src/components/Modules/ClientWS.ts
+++ src/renderer/src/components/Modules/ClientWS.ts
@@ -1,115 +1,205 @@
 import Websocket from 'isomorphic-ws';
 
 class ReconnectTimer {
-  static timerID: NodeJS.Timeout | undefined;
-  // первоначальное значение timeout
-  static initialTimeout: number = 5000;
+  private timerID: NodeJS.Timeout | undefined;
+  // первоначальное значение timeout (в мс)
+  private initialTimeout: number = 5000;
   // на сколько мс увеличивается время перед новой попыткой подключения
-  static incTimeout: number = this.initialTimeout;
+  private incTimeout: number = this.initialTimeout;
   /**  
     максимальное количество автоматических попыток переподключения,
     значение меньше нуля означает, что ограничения на попытки отсутствует
   */
-  static maxReconnectAttempts: number = 3;
+  private maxReconnectAttempts: number = 3;
   // количество совершённых попыток переподключения, сбрасывается при удачном подключении или при смене хоста
-  static curReconnectAttemps: number = 0;
+  private curReconnectAttemps: number = 0;
   /**  
   максимальное количество мс, через которое клиент будет пытаться переподключиться,
   не должно быть негативным числом (поэтому не стоит делать эту переменную зависимой от maxReconnectAttempts)
   */
-  static maxTimeout: number = this.incTimeout * 10;
-  static timeout: number = this.initialTimeout;
+  private maxTimeout: number = this.incTimeout * 10;
+  private curTimeout: number = this.initialTimeout;
+  private freezeReconnection: boolean = false;
+  // true = пытаться переподключиться автоматически
+  private autoReconnect: boolean = false;
+  // true = соединение было отменено пользователем и переподключаться не нунжо.
+  private connectionCanceled: boolean = false;
+
+  constructor(
+    initialTimeout: number = 5000,
+    incTimeout: number = 5000,
+    maxReconnectAttempts: number = 3,
+    maxTimeout: number = 50000,
+    autoReconnect: boolean = true
+  ) {
+    this.initialTimeout = initialTimeout;
+    this.curTimeout = initialTimeout;
+    this.maxTimeout = maxTimeout;
+    this.incTimeout = incTimeout;
+
+    this.maxReconnectAttempts = maxReconnectAttempts;
+    this.curReconnectAttemps = 0;
+
+    this.autoReconnect = autoReconnect;
+    this.freezeReconnection = false;
+    this.connectionCanceled = false;
+  }
+
+  setTimerID(timerID: NodeJS.Timeout | undefined) {
+    this.timerID = timerID;
+  }
+
+  setReconnection(reconnection: boolean) {
+    this.autoReconnect = reconnection;
+  }
+
+  cancelConnection() {
+    this.connectionCanceled = true;
+    this.curReconnectAttemps = 0;
+    this.clearTimer();
+  }
+
+  private clearTimer() {
+    if (this.timerID) {
+      clearTimeout(this.timerID);
+      this.timerID = undefined;
+    }
+  }
+
+  reset(autoReconnect: boolean = true) {
+    this.clearTimer();
+
+    this.curTimeout = this.initialTimeout;
+    this.curReconnectAttemps = 0;
+
+    this.autoReconnect = autoReconnect;
+    this.freezeReconnection = false;
+    this.connectionCanceled = false;
+  }
+
+  freeze(freeze: boolean) {
+    this.freezeReconnection = freeze;
+  }
+
+  autoReconnectSet(autoReconnect: boolean) {
+    this.autoReconnect = autoReconnect;
+  }
+
+  IsAutoReconnect(): boolean {
+    return this.autoReconnect;
+  }
+
+  incReconnectAttempt() {
+    this.curReconnectAttemps = Math.max(this.curReconnectAttemps + 1, this.maxReconnectAttempts);
+  }
+
+  // переподключение по таймеру
+  tryToReconnect(reconnectFunction: () => void) {
+    if (
+      this.connectionCanceled ||
+      (this.maxReconnectAttempts >= 0 && this.curReconnectAttemps >= this.maxReconnectAttempts)
+    ) {
+      return;
+    }
+    this.timerID = setTimeout(() => {
+      console.log(`inTimer: ${this.curTimeout}`);
+      if (!this.freezeReconnection) {
+        this.curTimeout = Math.min(this.curTimeout + this.incTimeout, this.maxTimeout);
+        this.curReconnectAttemps++;
+        reconnectFunction();
+      } else {
+        console.log('the timer is frozen');
+        if (this.curTimeout == 0) {
+          this.curTimeout = Math.min(this.incTimeout, this.maxTimeout);
+          this.tryToReconnect(reconnectFunction);
+        } else {
+          this.tryToReconnect(reconnectFunction);
+        }
+      }
+    }, this.curTimeout);
+  }
+}
+
+class ClientStatus {
+  static CONNECTING: string = 'Идет подключение...';
+  static CONNECTED: string = 'Подключен';
+  static NO_CONNECTION: string = 'Не подключен';
+  static CONNECTION_ERROR = 'Ошибка при попытке подключиться';
 }
 
 export abstract class ClientWS {
   static port: number;
   static host: string;
-  // хост + порт
-  static base_address: string;
   // соединение с удалённым сервером, если оно отсутстует, то undefined
   static connection: Websocket | undefined;
   // true - в данный момент идёт подключение
-  static connecting: boolean = false;
-  static reconnectTimer: ReconnectTimer;
-  // true = во время вызова таймера для переключения ничего не будет происходить.
-  private static freezeReconnection: boolean = false;
-  // true = пытаться переподключиться автоматически
-  private static reconnection: boolean = false;
-  // true = соединение было отменено пользователем и переподключаться не нунжо.
-  private static connectionCanceled: boolean = false;
+  // static connecting: boolean = false;
+  static reconnectTimer: ReconnectTimer = new ReconnectTimer();
+
+  static setConnectionStatus: (newConnectionStatus: string) => void;
+
+  static bindReact(setConnectionStatus: (newConnectionStatus: string) => void): void {
+    this.setConnectionStatus = setConnectionStatus;
+  }
 
   static checkConnection(): boolean {
     return this.connection !== undefined;
   }
 
-  /** переподключение к последнему адресом к которому клиент пытался подключиться*/
+  /**
+   * переподключение к последнему адресом к которому клиент пытался подключиться
+   */
   static reconnect() {
+    this.reconnectTimer.incReconnectAttempt();
     this.connect(this.host, this.port);
   }
 
-  /** 
-   подключение к заданному хосту и порту, если оба параметра не заданы, то идёт подключение к локальному хосту, если только один из параметров задан, то меняется только тот параметр, что был задан.
-  */
+  /**
+   * подключение к заданному хосту и порту, отключается от предыдущего адреса
+   */
   static async connect(host: string, port: number): Promise<Websocket | undefined> {
-    if (this.connecting) return;
-    this.connecting = true;
-    //this.setFlasherConnectionStatus(FLASHER_CONNECTING);
-    //this.clearTimer();
-
+    // чтобы предовратить повторное соединение
+    if (this.connection && this.connection.CONNECTING) return;
+    // проверяем, что адрес является новым
+    if (this.host != host || this.port != port) {
+      this.reconnectTimer.reset();
+      // проверяем, что не идёт переподключения к текущему соединению
+    } else if (this.connection && this.connection.OPEN) {
+      return;
+    }
+    this.setConnectionStatus(ClientStatus.CONNECTING);
+    /*
+      перед отключением нужно глобально поменять значения хоста и порта, 
+      чтобы клиент не пытался подключиться обратно
+    */
     this.host = host;
     this.port = port;
-
-    const new_address = this.makeAddress(this.host, this.port);
-    // означает, что хост должен смениться
-    if (new_address != this.base_address) {
-      //Flasher.curReconnectAttemps = 1;
-      this.base_address = new_address;
-      //Flasher.timeout = Flasher.initialTimeout;
-    }
-    host = this.host;
-    port = this.port;
     this.connection?.close();
-    //this.setFlasherDevices(new Map());
-    this.connectionCanceled = false;
 
     let ws: Websocket;
     try {
-      ws = new Websocket(this.base_address);
+      ws = new Websocket(this.makeAddress(host, port));
       this.connection = ws;
-      //this.setErrorMessage(undefined);
     } catch (error) {
-      //this.setErrorMessage(`${error}`);
-      //console.log('Flasher websocket error', error);
-      //this.setFlasherConnectionStatus(FLASHER_CONNECTION_ERROR);
-      this.end();
+      this.errorHandler(error);
       return;
     }
 
     ws.onopen = () => {
-      //ReconnectTimer.curReconnectAttemps = 0;
-      //console.log(`Flasher: connected to ${Flasher.host}:${Flasher.port}!`);
-      //this.setFlasherConnectionStatus(FLASHER_CONNECTED);
+      this.reconnectTimer.reset();
+      console.log(`Client: connected to ${this.host}:${this.port}!`);
+      this.setConnectionStatus(ClientStatus.CONNECTED);
 
-      this.connecting = false;
       ws.onmessage = (msg: Websocket.MessageEvent) => {
         this.messageHandler(msg);
       };
     };
 
-    ws.onclose = async () => {
-      if (host == this.host && port == this.port) {
-        //this.setFlasherConnectionStatus(FLASHER_NO_CONNECTION);
-        this.end();
-      }
+    ws.onclose = async (event) => {
+      this.closeHandler(host, port, event);
     };
 
     return ws;
-  }
-
-  // действия после закрытии или ошибке соединения
-  private static end() {
-    this.connecting = false;
-    this.connection = undefined;
   }
 
   // получение адреса в виде строки
@@ -117,7 +207,24 @@ export abstract class ClientWS {
     return `ws://${host}:${port}`;
   }
 
+  // обработка входящих через вебсоект сообщений
   static messageHandler(msg: Websocket.MessageEvent) {
     return;
+  }
+
+  static closeHandler(host: string, port: number, event: Websocket.Event) {
+    if (host == this.host && port == this.port) {
+      this.setConnectionStatus(ClientStatus.NO_CONNECTION);
+      this.connection = undefined;
+      if (this.reconnectTimer.IsAutoReconnect()) {
+        this.reconnectTimer.tryToReconnect(this.reconnect);
+      }
+    }
+  }
+
+  static errorHandler(error) {
+    console.log('Websocket error', error);
+    this.setConnectionStatus(ClientStatus.CONNECTION_ERROR);
+    this.connection = undefined;
   }
 }

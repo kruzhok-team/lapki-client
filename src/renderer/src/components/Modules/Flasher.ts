b3434ea8a5fcaf00ceaab06f5a72a7318a693891
Roundabout1 <mrsivannap@gmail.com>
2024-08-07 17:26:18 +0800

    serial monitor: коды serialStatus

--- src/renderer/src/components/Modules/Flasher.ts
+++ src/renderer/src/components/Modules/Flasher.ts
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from 'react';
 
 import Websocket from 'isomorphic-ws';
+import { comment } from 'postcss';
 
 import { Binary } from '@renderer/types/CompilerTypes';
 import {
@@ -314,14 +315,92 @@ export class Flasher {
           }
           case 'serial-connection-status': {
             const serialStatus = response.payload as SerialStatus;
-            if (serialStatus.code == 0) {
-              //const dev = this.devices.get(serialStatus.deviceID);
-              //SerialMonitor.setDevice(dev);
-              SerialMonitor.addLog('Открыт монитор порта!');
-              SerialMonitor.setConnectionStatus(SERIAL_MONITOR_CONNECTED);
-            } else {
-              SerialMonitor.addLog(`Монитор порта закрыт. Код: ${serialStatus.code}`);
-              SerialMonitor.setConnectionStatus(SERIAL_MONITOR_NO_CONNECTION);
+            console.log(serialStatus);
+            switch (serialStatus.code) {
+              case 0:
+                SerialMonitor.addLog('Открыт монитор порта!');
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_CONNECTED);
+                break;
+              case 1:
+                if (serialStatus.comment != '') {
+                  SerialMonitor.addLog(
+                    `Монитор порта закрыт. Текст ошибки: ${serialStatus.comment}`
+                  );
+                } else {
+                  SerialMonitor.addLog(`Монитор порта закрыт.`);
+                }
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_NO_CONNECTION);
+                break;
+              case 2:
+                SerialMonitor.addLog(`Монитор порта закрыт, так как устройство не подключено.`);
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_NO_CONNECTION);
+                break;
+              case 3:
+                SerialMonitor.addLog(`Нельзя открыть монитор порта для фальшивого устройства.`);
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_NO_CONNECTION);
+                break;
+              case 4:
+                SerialMonitor.addLog(
+                  `Сервер не смог обработать JSON-сообщение от клиента. Текст ошибки: ${serialStatus.comment}`
+                );
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_NO_CONNECTION);
+                break;
+              case 5:
+                SerialMonitor.addLog(
+                  `Нельзя открыть монитор порта, так как устойство прошивается. Дождитесь окончания прошивки и повторите попытку.`
+                );
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_NO_CONNECTION);
+                break;
+              case 6:
+                SerialMonitor.addLog(`Монитор порта открыт другим клиентом.`);
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_NO_CONNECTION);
+                break;
+              case 7:
+                SerialMonitor.addLog(
+                  `Произошла ошибка чтения данных. Порт закрыт. Текст ошибки: ${serialStatus.comment}`
+                );
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_NO_CONNECTION);
+                break;
+              case 8:
+                SerialMonitor.addLog(`Монитор порта закрыт по вашему запросу.`);
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_NO_CONNECTION);
+                break;
+              case 9:
+                SerialMonitor.addLog(
+                  `Не удалось сменить скорость передачи данных. Соединение прервано. Выберите другую скорость и попробуйте снова.`
+                );
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_NO_CONNECTION);
+                break;
+              case 10:
+                SerialMonitor.addLog(
+                  `Монитор порта заново открыт на скорости ${serialStatus.comment} бод.`
+                );
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_CONNECTED);
+                break;
+              case 11:
+                SerialMonitor.addLog(
+                  `Не удалось сменить скорость передачи данных из-за ошибки обработки JSON-сообщения. Текст ошибки: ${serialStatus.comment}`
+                );
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_CONNECTED);
+                break;
+              case 12:
+                SerialMonitor.addLog(
+                  `Нельзя сменить скорость передачи данных, так как монитор порта закрыт.`
+                );
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_CONNECTED);
+                break;
+              case 13:
+                SerialMonitor.addLog(
+                  `Нельзя сменить скорость передачи данных, так как монитор порта открыт другим клиентом.`
+                );
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_CONNECTED);
+                break;
+              case 14:
+                SerialMonitor.addLog(
+                  `Этот монитор порта нельзя закрыть, так как он открыт другим клиентом.`
+                );
+                SerialMonitor.setConnectionStatus(SERIAL_MONITOR_CONNECTED);
+                break;
             }
             break;
           }
@@ -330,9 +409,7 @@ export class Flasher {
             if (serialStatus.code == 0) {
               SerialMonitor.addLog('Сообщение доставлено на устройство.');
             } else {
-              SerialMonitor.addLog(
-                `Сообщение не удалось доставить на устройство. Код: ${serialStatus.code}`
-              );
+              SerialMonitor.addLog(`Сообщение не удалось доставить на устройство.`);
             }
             break;
           }
@@ -345,6 +422,7 @@ export class Flasher {
     };
 
     ws.onclose = async (event) => {
+      SerialMonitor.setConnectionStatus(SERIAL_MONITOR_NO_CONNECTION);
       if (!event.wasClean) {
         if (this.connecting) {
           this.setErrorMessage(`Не удалось подключиться к серверу ${this.host}:${this.port}.`);

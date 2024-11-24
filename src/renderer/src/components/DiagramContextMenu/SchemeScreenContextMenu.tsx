3dc7b18bf87016cd0772841ed27dcd239893f0d6
L140-beep <kartoshka0302@mail.ru>
2024-11-24 19:32:00 +0800

    divide diagram context menu, add stub context menu for scheme screen

---
+++ src/renderer/src/components/DiagramContextMenu/SchemeScreenContextMenu.tsx
@@ -0,0 +1,8 @@
+/*
+  Контекстное меню при работе со схемотехническим экраном.
+*/
+export const SchemeScreenContextMenu: React.FC = () => {
+  // TODO(L140-beep): Контекстное меню для схемотехнического экрана
+  console.log('heree');
+  return <></>;
+};

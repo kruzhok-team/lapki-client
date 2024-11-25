c35ce15ddda08bfa4753259578d612400abe863b
Maxim Klypin <70793854+L140-beep@users.noreply.github.com>
2024-11-25 18:38:53 +0800

    Поддержка платформы МС-ТЮК (#428)
    TODO: шина в лампочнице
    TODO: разобраться с baudrate в шине
    
    --------------
    
    Co-authored-by: CodeR-na-r1 <86515995+CodeR-na-r1@users.noreply.github.com>
    Co-authored-by: Roundabout1 <86153878+Roundabout1@users.noreply.github.com>
    Co-authored-by: Vlad Sizykh <sizyh1375@gmail.com>
    Co-authored-by: Mikhail Chekan <chekoopa@mail.ru>

--- src/renderer/src/components/ComponentEditModal.tsx
+++ src/renderer/src/components/ComponentEditModal.tsx
@@ -114,7 +114,7 @@ export const ComponentEditModal: React.FC<ComponentEditModalProps> = ({
       onSide={handleDelete}
     >
       <ComponentFormFields
-        showMainData={!proto.singletone}
+        showMainData={!proto.singletone && !platform!.staticComponents}
         protoParameters={proto.constructorParameters}
         protoInitializationParameters={proto.initializationParameters}
         name={name}

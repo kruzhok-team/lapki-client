9d7cf4de7ce46eea3804cc2dbc71b70110762770
Maxim Klypin <70793854+L140-beep@users.noreply.github.com>
2024-11-28 17:11:29 +0700

    Убрано отображение виджета холста канваса на иных вкладках (#533)

--- src/renderer/src/components/EditorSettings.tsx
+++ src/renderer/src/components/EditorSettings.tsx
@@ -6,12 +6,15 @@ import { ReactComponent as ZoomIn } from '@renderer/assets/icons/zoom-in.svg';
 import { ReactComponent as ZoomOut } from '@renderer/assets/icons/zoom-out.svg';
 import { useSettings } from '@renderer/hooks/useSettings';
 import { useModelContext } from '@renderer/store/ModelContext';
+import { useTabs } from '@renderer/store/useTabs';
 
 export interface EditorSettingsProps {
   toggle: () => void;
 }
 
 export const EditorSettings: React.FC<EditorSettingsProps> = ({ toggle }) => {
+  const [activeTabName, items] = useTabs((state) => [state.activeTab, state.items]);
+  const activeTab = items.find((tab) => tab.name === activeTabName);
   const modelController = useModelContext();
   const headControllerId = modelController.model.useData('', 'headControllerId');
   const controller = modelController.controllers[headControllerId];
@@ -41,41 +44,43 @@ export const EditorSettings: React.FC<EditorSettingsProps> = ({ toggle }) => {
   if (!isMounted) return null;
 
   return (
-    <div className="absolute -left-60 bottom-3 flex items-stretch overflow-hidden rounded bg-bg-secondary">
-      <button
-        className="px-2 outline-none hover:bg-bg-hover active:bg-bg-active"
-        onClick={handleCanvasGrid}
-      >
-        <Grid width={20} height={20} />
-      </button>
+    activeTab?.type === 'editor' && (
+      <div className="absolute -left-60 bottom-3 flex items-stretch overflow-hidden rounded bg-bg-secondary">
+        <button
+          className="px-2 outline-none hover:bg-bg-hover active:bg-bg-active"
+          onClick={handleCanvasGrid}
+        >
+          <Grid width={20} height={20} />
+        </button>
 
-      <button
-        className="px-2 outline-none hover:bg-bg-hover active:bg-bg-active"
-        onClick={handleZoomOut}
-      >
-        <ZoomOut width={20} height={20} />
-      </button>
+        <button
+          className="px-2 outline-none hover:bg-bg-hover active:bg-bg-active"
+          onClick={handleZoomOut}
+        >
+          <ZoomOut width={20} height={20} />
+        </button>
 
-      <button
-        className="flex w-16 justify-center py-2 outline-none hover:bg-bg-hover active:bg-bg-active"
-        onClick={handleReset}
-      >
-        {Math.floor((1 / scale) * 100)}%
-      </button>
+        <button
+          className="flex w-16 justify-center py-2 outline-none hover:bg-bg-hover active:bg-bg-active"
+          onClick={handleReset}
+        >
+          {Math.floor((1 / scale) * 100)}%
+        </button>
 
-      <button
-        className="px-2 outline-none hover:bg-bg-hover active:bg-bg-active"
-        onClick={handleZoomIn}
-      >
-        <ZoomIn width={20} height={20} />
-      </button>
+        <button
+          className="px-2 outline-none hover:bg-bg-hover active:bg-bg-active"
+          onClick={handleZoomIn}
+        >
+          <ZoomIn width={20} height={20} />
+        </button>
 
-      <button
-        className="px-2 text-primary outline-none hover:bg-bg-hover active:bg-bg-active"
-        onClick={toggle}
-      >
-        <Question height={20} width={20} />
-      </button>
-    </div>
+        <button
+          className="px-2 text-primary outline-none hover:bg-bg-hover active:bg-bg-active"
+          onClick={toggle}
+        >
+          <Question height={20} width={20} />
+        </button>
+      </div>
+    )
   );
 };

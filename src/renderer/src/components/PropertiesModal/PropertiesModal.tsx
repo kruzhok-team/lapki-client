72675e9dc8053228df1318c928b95cf8a02475f3
L140-beep <kartoshka0302@mail.ru>
2024-12-03 18:08:29 +0700

    add selector

--- src/renderer/src/components/PropertiesModal/PropertiesModal.tsx
+++ src/renderer/src/components/PropertiesModal/PropertiesModal.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { useForm } from 'react-hook-form';
 
-import { Modal } from '@renderer/components/UI';
+import { ReactComponent as StateMachineIcon } from '@renderer/assets/icons/cpu-bw.svg';
+import { Modal, Select, SelectOption } from '@renderer/components/UI';
 import { CanvasController } from '@renderer/lib/data/ModelController/CanvasController';
 import { getPlatform } from '@renderer/lib/data/PlatformLoader';
 import { useModelContext } from '@renderer/store/ModelContext';
-import { Meta as MetaData } from '@renderer/types/diagram';
+import { Meta as MetaData, StateMachine } from '@renderer/types/diagram';
 
 import { Meta, MetaFormValues } from './Meta';
 
@@ -31,14 +32,16 @@ export const PropertiesModal: React.FC<PropertiesModalProps> = ({
   const name = model.useData('', 'name');
   const basename = model.useData('', 'basename');
   // TODO(L140-beep): А вот здесь нужно селектор сделать и убрать CanvasController
-  const stateMachines = Object.keys(controller.stateMachinesSub);
-  const currentSm = stateMachines[0];
-  const platform = model.useData(currentSm, 'elements.platform');
-  const meta: MetaData = model.useData(currentSm, 'elements.meta');
-  const [properties, setProperties] = useState<[string, string][]>([]);
-
+  const stateMachines = modelController.model.useData('', 'elements.stateMachinesId') as {
+    [id: string]: StateMachine;
+  };
+  const stateMachinesId = Object.keys(stateMachines).filter((value) => value !== '');
+  const [selectedSm, setSelectedSm] = useState<string>(stateMachinesId[0]);
+  const platform = model.useData(selectedSm, 'elements.platform');
+  const meta: MetaData = model.useData(selectedSm, 'elements.meta');
+  const [baseProperties, setBaseProperties] = useState<[string, string][]>([]);
   const metaForm = useForm<MetaFormValues>();
-  const onAfterOpen = async () => {
+  useMemo(async () => {
     metaForm.setValue(
       'meta',
       Object.entries(meta).map(([name, value]) => ({ name, value })) as never // TODO(L140-beep): Почему линтер ругается?
@@ -57,12 +60,25 @@ export const PropertiesModal: React.FC<PropertiesModalProps> = ({
       propertiesValues.push(['Дата и время создания файла', dateFormat(stat['birthtime'])]);
       propertiesValues.push(['Размер файла', stat['size'] + ' байтов']);
     }
-    setProperties(propertiesValues);
-  };
+    setBaseProperties(propertiesValues);
+  }, [basename, name, selectedSm]);
+
+  const smOptions: SelectOption[] = useMemo(() => {
+    const getOption = (id: string) => {
+      return {
+        value: id,
+        label: id,
+        hint: undefined,
+        icon: <StateMachineIcon className="size-8 fill-border-contrast px-1" />,
+      };
+    };
+
+    return stateMachinesId.map((smId) => getOption(stateMachines[smId].name ?? smId));
+  }, [stateMachines, stateMachinesId]);
 
   const handleMetaSubmit = metaForm.handleSubmit((data) => {
     model.setMeta(
-      currentSm,
+      selectedSm,
       data.meta.reduce((acc, cur) => {
         acc[cur.name] = cur.value;
 
@@ -73,23 +89,37 @@ export const PropertiesModal: React.FC<PropertiesModalProps> = ({
     onClose();
   });
 
+  const handleStateMachineChange = async (smId: string) => {
+    if (!smId) return;
+    setSelectedSm(smId);
+    // onAfterOpen();
+    // // setSmProperties(Object.entries(meta).map(([name, value]) => [name, value]));
+  };
+
   return (
     <Modal
       {...props}
       onRequestClose={onClose}
-      onAfterOpen={onAfterOpen}
+      // onAfterOpen={onAfterOpen}
       onSubmit={handleMetaSubmit}
       title="Свойства"
     >
       <h3 className="mb-1 text-xl">Свойства файла</h3>
+      <Select
+        containerClassName="w-[250px]"
+        options={smOptions}
+        onChange={(opt) => handleStateMachineChange(opt?.value ?? '')}
+        value={smOptions.find((o) => o.value === selectedSm) ?? null}
+        isSearchable={false}
+        noOptionsMessage={() => 'Нет подходящих атрибутов'}
+      />
       <div className="mb-2 flex flex-col gap-1">
-        {properties.map(([name, value]) => (
+        {[...baseProperties].map(([name, value]) => (
           <div key={name}>
             <b>{name}:</b> {value}
           </div>
         ))}
       </div>
-
       <Meta form={metaForm} />
     </Modal>
   );

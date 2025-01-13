3fafb0ca020f11ca129d6c3147dbed072329bf77
L140-beep <kartoshka0302@mail.ru>
2025-01-13 16:17:02 +0700

    typechecker war 2

--- src/renderer/src/components/NodeModal/TransitionModal.tsx
+++ src/renderer/src/components/NodeModal/TransitionModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import { Modal } from '@renderer/components/UI';
 import { useModal } from '@renderer/hooks/useModal';

9f13aa0981cd37df9de22e8669f7ccf993d45015
Mikhail Chekan <chekoopa@mail.ru>
2025-01-14 18:04:05 +0800

    Переработка начального экрана
    Добавлен вывод версии
    Горячие клавиши перевёрстаны в таблицу

--- src/renderer/src/components/MainContainer/NotInitialized.tsx
+++ src/renderer/src/components/MainContainer/NotInitialized.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
 
 import { ReactComponent as Icon } from '@renderer/assets/icons/icon.svg';
+import { appVersion } from '@renderer/version';
 
 const combination = [
   {
-    name: 'Создать файл',
+    name: 'Создать новую схему',
     command: {
       button1: 'Ctrl',
       button2: 'N',
     },
   },
   {
-    name: 'Открыть файл',
+    name: 'Открыть файл схемы',
     command: {
       button1: 'Ctrl',
       button2: 'O',
     },
   },
   {
-    name: 'Импорт схемы (Cyberiada-GraphML)',
+    name: 'Импорт схемы из другого формата',
     command: {
       button1: 'Ctrl',
       button2: 'I',
     },
   },
   {
-    name: 'Справка',
+    name: 'Открыть справку',
     command: {
       button1: 'F1',
       button2: undefined,
@@ -44,29 +45,33 @@ export const NotInitialized: React.FC = () => {
   return (
     <div className="flex flex-col items-center pt-24">
       <Icon />
+      <br />
+      <p className="text-center text-2xl font-bold">Lapki IDE v{appVersion}</p>
       <p className="py-6 text-center text-base">
         Перетащите файл в эту область или воспользуйтесь комбинацией клавиш:
       </p>
-      <div>
-        {combination.map((value, key) => (
-          <div key={key} className="my-3 flex justify-between">
-            <div className="px-1">{value.name}</div>
-            <div className="flex items-start">
-              <div className="rounded border-b-2 bg-gray-600 px-1 text-gray-300">
-                {value.command.button1}
-              </div>
-              {value.command.button2 && (
-                <>
-                  <p className="px-1">+</p>
-                  <div className="rounded border-b-2 bg-gray-600 px-1 text-gray-300">
-                    {value.command.button2}
-                  </div>
-                </>
-              )}
-            </div>
-          </div>
-        ))}
-      </div>
+      <table>
+        <tbody>
+          {combination.map((value, key) => (
+            <tr key={key}>
+              <td className="px-1 py-1">{value.name}</td>
+              <td className="ml-3 flex items-start py-1">
+                <div className="rounded border-b-2 bg-gray-600 px-1 text-gray-300">
+                  {value.command.button1}
+                </div>
+                {value.command.button2 && (
+                  <>
+                    <p className="px-1">+</p>
+                    <div className="rounded border-b-2 bg-gray-600 px-1 text-gray-300">
+                      {value.command.button2}
+                    </div>
+                  </>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };

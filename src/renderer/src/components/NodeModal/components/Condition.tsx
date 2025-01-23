8350efecf8c90aeb503d0f00e7afe6e013e2d045
Roundabout1 <mrsivannap@gmail.com>
2025-01-23 19:27:42 +0800

    переключатель константа-атрибут

--- src/renderer/src/components/NodeModal/components/Condition.tsx
+++ src/renderer/src/components/NodeModal/components/Condition.tsx
@@ -10,6 +10,7 @@ import { useModelContext } from '@renderer/store/ModelContext';
 import { useCondition } from '../hooks';
 
 import '../style.css';
+import { AttributeConstSwitch } from '@renderer/components/AttributeConstSwitch';
 
 const operand = [
   {
@@ -147,15 +148,16 @@ export const Condition: React.FC<ConditionProps> = memo(function Condition(props
         <TabPanel value={0} tabValue={tabValue}>
           <div className="flex flex-col gap-2">
             <div className="flex items-start">
-              <Checkbox
-                checked={!isParamOneInput1}
-                onCheckedChange={(v) => handleParamOneInput1(!v)}
-                className="mr-2 mt-[9px]"
-              />
+              <div className="mr-2 mt-[2px]">
+                <AttributeConstSwitch
+                  defaultIsAttribute={isParamOneInput1}
+                  beforeSwitch={(v) => handleParamOneInput1(!v)}
+                ></AttributeConstSwitch>
+              </div>
               {isParamOneInput1 ? (
-                <div className="flex w-full gap-2">
+                <div className="flex gap-2">
                   <Select
-                    containerClassName="w-full"
+                    containerClassName="w-[305px]"
                     options={componentOptionsParam1}
                     onChange={handleComponentParam1Change}
                     value={
@@ -166,7 +168,7 @@ export const Condition: React.FC<ConditionProps> = memo(function Condition(props
                     error={errors.selectedComponentParam1 || ''}
                   />
                   <Select
-                    containerClassName="w-full"
+                    containerClassName="w-[305px]"
                     options={methodOptionsParam1}
                     onChange={handleMethodParam1Change}
                     value={
@@ -189,7 +191,7 @@ export const Condition: React.FC<ConditionProps> = memo(function Condition(props
             </div>
 
             <Select
-              containerClassName="pl-7"
+              containerClassName="pl-20"
               className="max-w-[220px]"
               placeholder="Выберите оператор"
               options={operand}
@@ -199,15 +201,16 @@ export const Condition: React.FC<ConditionProps> = memo(function Condition(props
             />
 
             <div className="flex items-start">
-              <Checkbox
-                checked={!isParamOneInput2}
-                onCheckedChange={(v) => handleParamOneInput2(!v)}
-                className="mr-2 mt-[9px]"
-              />
+              <div className="mr-2 mt-[2px]">
+                <AttributeConstSwitch
+                  defaultIsAttribute={isParamOneInput2}
+                  beforeSwitch={(v) => handleParamOneInput2(!v)}
+                ></AttributeConstSwitch>
+              </div>
               {isParamOneInput2 ? (
-                <div className="flex w-full gap-2">
+                <div className="flex gap-2">
                   <Select
-                    containerClassName="w-full"
+                    containerClassName="w-[305px]"
                     options={componentOptionsParam2}
                     onChange={handleComponentParam2Change}
                     value={
@@ -218,7 +221,7 @@ export const Condition: React.FC<ConditionProps> = memo(function Condition(props
                     error={errors.selectedComponentParam2 || ''}
                   />
                   <Select
-                    containerClassName="w-full"
+                    containerClassName="w-[305px]"
                     options={methodOptionsParam2}
                     onChange={handleMethodParam2Change}
                     value={

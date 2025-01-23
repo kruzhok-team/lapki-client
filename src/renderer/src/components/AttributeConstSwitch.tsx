284800490875b4d0ae38627f4557b104b2210e6a
Roundabout1 <mrsivannap@gmail.com>
2025-01-23 14:32:56 +0800

    подсказки

--- src/renderer/src/components/AttributeConstSwitch.tsx
+++ src/renderer/src/components/AttributeConstSwitch.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import { twMerge } from 'tailwind-merge';
 
+import { WithHint } from './UI';
+
 interface AttributeConstSwitch {
   isAttributeSelected?: boolean;
   hidden?: boolean;
@@ -18,24 +20,37 @@ export const AttributeConstSwitch: React.FC<AttributeConstSwitch> = ({
     isAttributeOutside ?? false
   );
   const lowOpacity = 'opacity-20';
+  const selectOption = (
+    content: string,
+    selectCondtion: boolean,
+    setCondition: () => void,
+    hint: string
+  ) => {
+    return (
+      <WithHint hint={hint}>
+        {(hintProps) => (
+          <button
+            className={twMerge('btn-secondary px-2 py-1', !selectCondtion && lowOpacity)}
+            disabled={disabled ?? false}
+            onClick={() => setCondition()}
+            type="button"
+            {...hintProps}
+          >
+            {content}
+          </button>
+        )}
+      </WithHint>
+    );
+  };
   return (
     <div className="flex-row" {...props} hidden={hidden ?? false}>
-      <button
-        className={twMerge('btn-secondary px-2 py-1', !isAttributeInternal && lowOpacity)}
-        disabled={disabled ?? false}
-        onClick={() => setIsAttributeInternal(true)}
-        type="button"
-      >
-        ❎
-      </button>
-      <button
-        className={twMerge('btn-secondary px-2 py-1', isAttributeInternal && lowOpacity)}
-        disabled={disabled ?? false}
-        onClick={() => setIsAttributeInternal(false)}
-        type="button"
-      >
-        ✏
-      </button>
+      {selectOption(
+        '❎',
+        isAttributeInternal,
+        () => setIsAttributeInternal(true),
+        'Атрибут компонента'
+      )}
+      {selectOption('✏', !isAttributeInternal, () => setIsAttributeInternal(false), 'Константа')}
     </div>
   );
 };

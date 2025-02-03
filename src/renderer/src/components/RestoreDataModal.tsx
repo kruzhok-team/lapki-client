005bcb3421bacadf93d48bfd6a324233be689fff
Roundabout1 <mrsivannap@gmail.com>
2025-02-03 12:52:30 +0800

    Create RestoreDataModal.tsx

---
+++ src/renderer/src/components/RestoreDataModal.tsx
@@ -0,0 +1,42 @@
+import React, { FormEventHandler } from 'react';
+
+import { Modal } from './UI';
+
+interface RestoreDataModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onRestore: () => void;
+  onCancelRestore: () => void;
+}
+
+export const RestoreDataModal: React.FC<RestoreDataModalProps> = ({
+  onClose,
+  onRestore,
+  onCancelRestore,
+  ...props
+}) => {
+  const onRequestClose = () => {
+    onCancelRestore();
+    onClose();
+  };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onRestore();
+    onClose();
+  };
+  return (
+    <Modal
+      {...props}
+      onRequestClose={onRequestClose}
+      title="Восстановление схемы"
+      submitLabel="Да"
+      onSubmit={handleSubmit}
+      cancelLabel="Нет"
+      cancelClassName="btn-error"
+    >
+      Работа программы была прервана неожиданно. Хотите восстановить предыдущую схему?
+      <br></br>
+      <b>Внимание!</b> Если Вы выберете «Нет», то схема может быть утеряна навсегда!
+    </Modal>
+  );
+};

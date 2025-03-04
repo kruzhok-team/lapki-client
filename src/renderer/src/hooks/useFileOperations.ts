99665085880cb3b22f429aecb35465c00b85991f
Roundabout1 <mrsivannap@gmail.com>
2025-03-04 11:48:07 +0800

    удаление недавних файлов

--- src/renderer/src/hooks/useFileOperations.ts
+++ src/renderer/src/hooks/useFileOperations.ts
@@ -31,7 +31,6 @@ export const useFileOperations = (args: useFileOperationsArgs) => {
   const isStale = modelController.model.useData('', 'isStale');
   const [clearTabs, openTab] = useTabs((state) => [state.clearTabs, state.openTab]);
   const [restoreSession, setRestoreSession] = useSettings('restoreSession');
-  const [recentFiles, setRecentFiles] = useSettings('recentFiles');
 
   const [data, setData] = useState<SaveModalData | null>(null);
   const [isOpen, setIsOpen] = useState(false);
@@ -260,7 +259,7 @@ export const useFileOperations = (args: useFileOperationsArgs) => {
     return () => {
       unsubscribe();
     };
-  }, [handleSaveFile, model, recentFiles]);
+  }, [handleSaveFile, model]);
 
   return {
     saveModalProps: { isOpen, onClose, data, deleteTempSave },

f4e2d67cb839c37faa981ea3858fa6e05ad5bdd9
Roundabout1 <mrsivannap@gmail.com>
2025-03-04 11:48:28 +0800

    хуки для запоминания недавних файлов

---
+++ src/renderer/src/hooks/useRecentFilesHooks.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+import { useModelContext } from '@renderer/store/ModelContext';
+import { StateMachine } from '@renderer/types/diagram';
+
+import { useSettings } from './useSettings';
+
+export const useRecentFilesHooks = () => {
+  const [recentFiles, setRecentFiles] = useSettings('recentFiles');
+
+  const modelController = useModelContext();
+  const name = modelController.model.useData('', 'name') as string | null;
+  const basename = modelController.model.useData('', 'basename');
+  const stateMachinesId = modelController.model.useData('', 'elements.stateMachinesId') as {
+    [ID: string]: StateMachine;
+  };
+
+  const [settingsLoaded, setSettingsLoaded] = useState<boolean>(recentFiles !== null);
+
+  useEffect(() => {
+    setSettingsLoaded(recentFiles !== null);
+  }, [recentFiles]);
+
+  useEffect(() => {
+    if (recentFiles === null || name === null || basename === null) {
+      return;
+    }
+
+    const stateMachines = [...Object.entries(stateMachinesId)].map(([smId, sm]) => {
+      return { name: sm.name ?? smId, platformIdx: sm.platform };
+    });
+
+    const filtered = recentFiles.filter((v) => v.path !== basename);
+    setRecentFiles([...filtered, { name: name, path: basename, stateMachines: stateMachines }]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [basename, name, stateMachinesId, settingsLoaded]);
+};

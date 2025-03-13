41c66c106c2fb4a7772d8246c121908ecb673743
Roundabout1 <mrsivannap@gmail.com>
2025-03-13 13:08:30 +0800

    отделение тригерров от чекбокса

--- src/renderer/src/components/Sidebar/Flasher/AddressBookRow.tsx
+++ src/renderer/src/components/Sidebar/Flasher/AddressBookRow.tsx
@@ -22,35 +22,37 @@ export const AddressBookRow: React.FC<AddressBookRowProps> = (props) => {
     props;
   const labelClassName = twMerge('flex w-full', isSelected && 'bg-bg-active', cellHeight);
   return (
-    <div
-      className="flex items-start"
-      draggable
-      onClick={onSelect}
-      onDoubleClick={onEdit}
-      onDragStart={onDragStart}
-      onDrop={onDrop}
-    >
+    <div className="flex items-start">
       <Checkbox
         checked={isChecked}
         onCheckedChange={onCheckChange}
-        className={twMerge('w-[76px] rounded border border-border-primary', cellHeight)}
+        className={twMerge('w-[24px] rounded border border-border-primary', cellHeight)}
       />
-      <label className={labelClassName}>
-        <TextInput value={data.name ?? ''} disabled={true} placeholder="Название" />
-      </label>
+      <div
+        className="flex items-start"
+        draggable
+        onClick={onSelect}
+        onDoubleClick={onEdit}
+        onDragStart={onDragStart}
+        onDrop={onDrop}
+      >
+        <label className={labelClassName}>
+          <TextInput value={data.name ?? ''} disabled={true} placeholder="Название" />
+        </label>
 
-      <label className={labelClassName}>
-        <TextInput value={data.address} disabled={true} />
-      </label>
+        <label className={labelClassName}>
+          <TextInput value={data.address} disabled={true} />
+        </label>
 
-      <label className={labelClassName}>
-        <TextInput
-          value={data.type ?? ''}
-          placeholder="Тип"
-          className="w-full max-w-full"
-          disabled={true}
-        />
-      </label>
+        <label className={labelClassName}>
+          <TextInput
+            value={data.type ?? ''}
+            placeholder="Тип"
+            className="w-full max-w-full"
+            disabled={true}
+          />
+        </label>
+      </div>
     </div>
   );
 };

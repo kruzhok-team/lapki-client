e805ba8b70020e05dd48d2aaad1e1ea72afa9098
Roundabout1 <mrsivannap@gmail.com>
2025-03-14 11:06:23 +0800

    новый стиль

--- src/renderer/src/components/Sidebar/Flasher/MetaData.tsx
+++ src/renderer/src/components/Sidebar/Flasher/MetaData.tsx
@@ -1,3 +1,4 @@
+import { ComponentFormFieldLabel } from '@renderer/components/ComponentFormFieldLabel';
 import { ManagerMS } from '@renderer/components/Modules/ManagerMS';
 import { Modal } from '@renderer/components/UI';
 import { AddressData } from '@renderer/types/FlasherTypes';
@@ -14,6 +15,16 @@ export const MetaDataModal: React.FC<MetaDataModalProps> = ({ addressData, isOpe
     //TODO
     onClose();
   };
+  const renderField = (label: string, value: string) => {
+    return (
+      <ComponentFormFieldLabel
+        label={`${label}:`}
+        value={value}
+        disabled={true}
+        className="rounded-none border-b border-l-0 border-r-0 border-t-0 pb-1"
+      />
+    );
+  };
   const meta = addressData.meta;
   return (
     <Modal
@@ -24,31 +35,24 @@ export const MetaDataModal: React.FC<MetaDataModalProps> = ({ addressData, isOpe
       submitLabel={'Скопировать метаданные'}
     >
       {meta && (
-        <div className="mb-2 flex flex-col gap-1">
-          <h3 className="mb-1 text-xl">Метаданные</h3>
-          <div>
-            <b>{'bootloader REF_HW'}:</b> {meta.RefBlHw}
+        <div className="flex flex-col gap-2">
+          <label>
+            <b>Bootloader:</b>
+          </label>
+          <div className="ml-2 flex flex-col gap-2">
+            {renderField('REF_HW', meta.RefBlHw)}
+            {renderField('REF_FW', meta.RefBlFw)}
+            {renderField('REF_CHIP', meta.RefBlChip)}
+            {renderField('REF_PROTOCOL', meta.RefBlProtocol)}
+            {renderField('USER_CODE', meta.RefBlUserCode)}
           </div>
-          <div>
-            <b>{'bootloader REF_FW'}:</b> {meta.RefBlFw}
-          </div>
-          <div>
-            <b>{'bootloader REF_CHIP'}:</b> {meta.RefBlChip}
-          </div>
-          <div>
-            <b>{'bootloader REF_PROTOCOL'}:</b> {meta.RefBlProtocol}
-          </div>
-          <div>
-            <b>{'bootloader USER_CODE'}:</b> {meta.RefBlUserCode}
-          </div>
-          <div>
-            <b>{'cybergene REF_FW'}:</b> {meta.RefCgFw}
-          </div>
-          <div>
-            <b>{'cybergene REF_HW'}:</b> {meta.RefCgHw}
-          </div>
-          <div>
-            <b>{'cybergene REF_PROTOCOL'}:</b> {meta.RefCgProtocol}
+          <label>
+            <b>Cybergene:</b>
+          </label>
+          <div className="ml-2 flex flex-col gap-2">
+            {renderField('REF_HW', meta.RefCgHw)}
+            {renderField('REF_FW', meta.RefCgFw)}
+            {renderField('REF_PROTOCOL', meta.RefCgProtocol)}
           </div>
         </div>
       )}

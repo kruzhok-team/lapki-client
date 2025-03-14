94518ab6c8fe54405a046791cd907a279de0633d
Roundabout1 <mrsivannap@gmail.com>
2025-03-14 10:35:21 +0800

    удаление закоментировнного кода

--- src/renderer/src/components/Sidebar/Flasher/AddressEntryModal.tsx
+++ src/renderer/src/components/Sidebar/Flasher/AddressEntryModal.tsx
@@ -177,37 +177,6 @@ export const AddressEntryEditModal: React.FC<AddressEntryEditModalProps> = (prop
           }}
         />
       </div>
-      {/* <br></br>
-      {meta && (
-        <div className="mb-2 flex flex-col gap-1">
-          <h3 className="mb-1 text-xl">Метаданные</h3>
-          <div>
-            <b>{'bootloader REF_HW'}:</b> {meta.RefBlHw}
-          </div>
-          <div>
-            <b>{'bootloader REF_FW'}:</b> {meta.RefBlFw}
-          </div>
-          <div>
-            <b>{'bootloader REF_CHIP'}:</b> {meta.RefBlChip}
-          </div>
-          <div>
-            <b>{'bootloader REF_PROTOCOL'}:</b> {meta.RefBlProtocol}
-          </div>
-          <div>
-            <b>{'bootloader USER_CODE'}:</b> {meta.RefBlUserCode}
-          </div>
-          <div>
-            <b>{'cybergene REF_FW'}:</b> {meta.RefCgFw}
-          </div>
-          <div>
-            <b>{'cybergene REF_HW'}:</b> {meta.RefCgHw}
-          </div>
-          <div>
-            <b>{'cybergene REF_PROTOCOL'}:</b> {meta.RefCgProtocol}
-          </div>
-        </div>
-      )}
-      {!meta && <p className="mb-1 text-xl opacity-60">Метаданных нет</p>} */}
     </Modal>
   );
 };

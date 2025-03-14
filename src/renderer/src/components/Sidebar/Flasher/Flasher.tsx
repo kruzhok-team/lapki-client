e4abca3898bed4fd65a94099457d28f0a3399e89
Roundabout1 <mrsivannap@gmail.com>
2025-03-14 11:50:56 +0800

    добавление пропущенной строчки метаданных

--- src/renderer/src/components/Sidebar/Flasher/Flasher.tsx
+++ src/renderer/src/components/Sidebar/Flasher/Flasher.tsx
@@ -165,6 +165,7 @@ export const FlasherTab: React.FC = () => {
     - bootloader REF_FW: ${meta.RefBlFw}
     - bootloader REF_CHIP: ${meta.RefBlChip}
     - booloader REF_PROTOCOL: ${meta.RefBlProtocol}
+    - booloader USER_CODE: ${meta.RefBlUserCode}
     - cybergene REF_FW: ${meta.RefCgFw}
     - cybergene REF_HW: ${meta.RefCgHw}
     - cybergene REF_PROTOCOL: ${meta.RefCgProtocol}

adc02ae61db674bf9ad79034980efe23310b0e4d
L140-beep <kartoshka0302@mail.ru>
2025-03-27 18:02:59 +0700

    add platform

--- src/renderer/src/components/Modules/Flasher.ts
+++ src/renderer/src/components/Modules/Flasher.ts
@@ -69,7 +69,7 @@ export class Flasher extends ClientWS {
     if (device.isArduinoDevice()) {
       ending = 'ino.hex';
     } else if (device.isMSDevice() || device.isBlgMbDevice()) {
-      ending = '.bin';
+      ending = 'bin';
     } else {
       throw new Error('Попытка задать бинарные данные для неизвестной платформы!');
     }

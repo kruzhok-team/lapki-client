d93dba18040b4b1e8c8cc5f8b8f72028c0c0f6db
L140-beep <kartoshka0302@mail.ru>
2025-04-24 14:19:36 +0700

    add udev rules

---
+++ build/udev-rules.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Определяем файл для правил udev
+UDEV_RULES_FILE="/etc/udev/rules.d/99-usb.rules"
+
+# Проверяем, существует ли файл, если нет, создаем его
+if [ ! -f "$UDEV_RULES_FILE" ]; then
+    touch "$UDEV_RULES_FILE"
+fi
+
+# Добавляем правило в файл
+echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="ac01", GROUP="dialout", MODE="0666"' >> "$UDEV_RULES_FILE"
+
+# Перезагружаем правила udev
+udevadm control --reload-rules
+udevadm trigger
+
+echo "Правило успешно добавлено в $UDEV_RULES_FILE"

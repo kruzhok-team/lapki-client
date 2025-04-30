5f15a2deb38f8ba7eb45c735d528327027173dff
L140-beep <kartoshka0302@mail.ru>
2025-04-30 16:38:24 +0700

    add parameters validation

--- src/renderer/src/components/NodeModal/ActionsModal/ActionsModalParameters.tsx
+++ src/renderer/src/components/NodeModal/ActionsModal/ActionsModalParameters.tsx
@@ -132,9 +132,10 @@ export const ActionsModalParameters: React.FC<ActionsModalParametersProps> = ({
               label={label}
               labelClassName="whitespace-pre"
               hint={hint}
+              error={error}
+              childrenDivClassname="ml-[50px] w-[300px]"
             >
               <Select
-                className="w-[300px] pl-[50px]"
                 options={options}
                 value={options.find((o) => o.value === value)}
                 onChange={(opt) => handleInputChange(name, idx, opt?.value ?? '')}
@@ -204,7 +205,7 @@ export const ActionsModalParameters: React.FC<ActionsModalParametersProps> = ({
         }
         const methodOptions = methodOptionsSearch(selectedParameterComponent);
         return (
-          <div className="flex items-center space-x-2" key={name}>
+          <div className="flex space-x-2" key={name}>
             <div className="mt-[4px]">
               <AttributeConstSwitch
                 checked={currentChecked}

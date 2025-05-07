d99e5228ad913b050769a515698b0552348ab941
Roundabout1 <mrsivannap@gmail.com>
2025-05-07 10:35:37 +0800

    проба нового дизайна

--- src/renderer/src/components/AttributeConstSwitch.tsx
+++ src/renderer/src/components/AttributeConstSwitch.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import { Switch, WithHint } from './UI';
+import * as RSwitch from '@radix-ui/react-switch';
 
+import { WithHint } from './UI';
 interface AttributeConstSwitch {
   checked?: boolean;
   onCheckedChange?: (isAttribute: boolean) => void;
@@ -23,12 +24,25 @@ export const AttributeConstSwitch: React.FC<AttributeConstSwitch> = ({
       <WithHint hint={hint}>
         {(hintProps) => (
           <div {...hintProps}>
-            <Switch
+            {/* <Switch
               className={className}
               checked={checked}
               disabled={isDisabled}
               onCheckedChange={onCheckedChange}
-            />
+            /> */}
+            <RSwitch.Root
+              className={
+                'relative h-[25px] w-[42px] cursor-pointer rounded-full bg-bg-secondary shadow-[0_0_0_1px] shadow-border-primary outline-none'
+              }
+              id="airplane-mode"
+              checked={checked}
+              disabled={isDisabled}
+              onCheckedChange={onCheckedChange}
+            >
+              <RSwitch.Thumb className="block size-[21px] translate-x-0.5 rounded-full shadow-[0_0_0_1px] shadow-border-contrast transition duration-100 will-change-transform data-[state=checked]:translate-x-[19px]">
+                {checked ? 'А' : 'К'}
+              </RSwitch.Thumb>
+            </RSwitch.Root>
           </div>
         )}
       </WithHint>

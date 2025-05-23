8ddc1bdd20c258ebd1e50ed6b483ec6968ebb2b5
Roundabout1 <mrsivannap@gmail.com>
2025-05-23 12:54:22 +0800

    исправление сброса таблицы прошивок

--- src/renderer/src/hooks/useFlasherHooks.ts
+++ src/renderer/src/hooks/useFlasherHooks.ts
@@ -12,7 +12,6 @@ import {
   SerialMonitor,
 } from '@renderer/components/Modules/SerialMonitor';
 import { ClientStatus } from '@renderer/components/Modules/Websocket/ClientStatus';
-import { useModelContext } from '@renderer/store/ModelContext';
 import { useFlasher } from '@renderer/store/useFlasher';
 import { useManagerMS } from '@renderer/store/useManagerMS';
 import { useSerialMonitor } from '@renderer/store/useSerialMonitor';
@@ -32,9 +31,6 @@ import {
 import { useSettings } from './useSettings';
 
 export const useFlasherHooks = () => {
-  const modelController = useModelContext();
-  const basename = modelController.model.useData('', 'basename');
-
   const [flasherSetting, setFlasherSetting] = useSettings('flasher');
   const [monitorSetting] = useSettings('serialmonitor');
 
@@ -51,8 +47,6 @@ export const useFlasherHooks = () => {
     connectionStatus,
     setErrorMessage,
     setHasAvrdude,
-    flashTableData,
-    setFlashTableData,
     binaryFolder,
     setBinaryFolder,
   } = useFlasher();
@@ -242,18 +236,6 @@ export const useFlasherHooks = () => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [connectionStatus]);
-
-  useEffect(() => {
-    setFlashTableData(
-      flashTableData.map((item) => {
-        return {
-          ...item,
-          source: undefined,
-          isFile: false,
-        };
-      })
-    );
-  }, [basename]);
 
   useEffect(() => {
     if (flasherMessage === null) return;

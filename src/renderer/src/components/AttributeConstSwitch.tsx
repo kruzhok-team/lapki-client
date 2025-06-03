6837175cfa8fad9fff6758d8b6afe1b0ad3933f5
Roundabout1 <mrsivannap@gmail.com>
2025-06-03 18:08:14 +0800

    WIP: атрибут-константа

--- src/renderer/src/components/AttributeConstSwitch.tsx
+++ src/renderer/src/components/AttributeConstSwitch.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
-import * as RSwitch from '@radix-ui/react-switch';
+import Switch from 'react-switch';
 import { twMerge } from 'tailwind-merge';
 
 import { ReactComponent as AttributeIcon } from '@renderer/assets/icons/useAttribute.svg';
 import { ReactComponent as ConstIcon } from '@renderer/assets/icons/useConst.svg';
+import { getColor } from '@renderer/theme';
 
 import { WithHint } from './UI';
 interface AttributeConstSwitch {
-  isAttribute?: boolean;
-  onCheckedChange?: (isAttribute: boolean) => void;
+  isAttribute: boolean;
+  onCheckedChange: (isAttribute: boolean) => void;
   className?: string;
   isDisabled?: boolean;
 }
@@ -27,12 +28,36 @@ export const AttributeConstSwitch: React.FC<AttributeConstSwitch> = ({
     }
     return null;
   };
+  // const color = getColor('bg-secondary');
+  const color = '#F1F1F1';
+  // console.log(color);
   return (
     <div {...props}>
       <WithHint hint={getHint()}>
         {(hintProps) => (
           <div {...hintProps}>
-            <RSwitch.Root
+            <Switch
+              className={twMerge(
+                'bg-bg-secondary shadow-[0_0_0_1px] shadow-border-primary outline-none focus:shadow-[0_0_0_1px]',
+                className
+              )}
+              onChange={(checked) => onCheckedChange(checked)}
+              checked={isAttribute}
+              disabled={isDisabled}
+              checkedIcon={
+                <ConstIcon className="absolute left-0.5 top-[10%] size-[23px] opacity-40" />
+              }
+              checkedHandleIcon={<AttributeIcon />}
+              uncheckedIcon={
+                <AttributeIcon className=" absolute right-0.5 top-[10%] size-[23px] opacity-40" />
+              }
+              uncheckedHandleIcon={<ConstIcon />}
+              onColor={color}
+              offColor={color}
+              handleDiameter={24}
+              //borderRadius={21}
+            />
+            {/* <RSwitch.Root
               className={twMerge(
                 'relative h-[25px] w-[42px] cursor-pointer rounded-full bg-bg-secondary shadow-[0_0_0_1px] shadow-border-primary outline-none focus:shadow-[0_0_0_1px]',
                 className
@@ -45,7 +70,7 @@ export const AttributeConstSwitch: React.FC<AttributeConstSwitch> = ({
               <RSwitch.Thumb className="block size-[21px] translate-x-0.5 rounded-full shadow-[0_0_0_1px] shadow-border-contrast transition duration-100 will-change-transform data-[state=checked]:translate-x-[19px]">
                 {isAttribute ? <AttributeIcon /> : <ConstIcon />}
               </RSwitch.Thumb>
-            </RSwitch.Root>
+            </RSwitch.Root> */}
           </div>
         )}
       </WithHint>

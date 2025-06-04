6aa58f71cde224dc9dcc87c5da80363a2ab4cfe6
Roundabout1 <mrsivannap@gmail.com>
2025-06-04 10:49:44 +0800

    удаление старого кода

--- src/renderer/src/components/AttributeConstSwitch.tsx
+++ src/renderer/src/components/AttributeConstSwitch.tsx
@@ -63,20 +63,6 @@ export const AttributeConstSwitch: React.FC<AttributeConstSwitch> = ({
               offColor={switchBgColor}
               handleDiameter={24}
             />
-            {/* <RSwitch.Root
-              className={twMerge(
-                'relative h-[25px] w-[42px] cursor-pointer rounded-full bg-bg-secondary shadow-[0_0_0_1px] shadow-border-primary outline-none focus:shadow-[0_0_0_1px]',
-                className
-              )}
-              id="airplane-mode"
-              checked={isAttribute}
-              disabled={isDisabled}
-              onCheckedChange={onCheckedChange}
-            >
-              <RSwitch.Thumb className="block size-[21px] translate-x-0.5 rounded-full shadow-[0_0_0_1px] shadow-border-contrast transition duration-100 will-change-transform data-[state=checked]:translate-x-[19px]">
-                {isAttribute ? <AttributeIcon /> : <ConstIcon />}
-              </RSwitch.Thumb>
-            </RSwitch.Root> */}
           </div>
         )}
       </WithHint>
